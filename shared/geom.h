762fc98d5470f2e2cc76d7afc324776181ddf55c
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2009-03-17 21:37:37 +0000

    check for antipodes when accumulating dual quats
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@687 07af1644-1164-4067-b897-63bf4dc9b748

--- shared/geom.h
+++ shared/geom.h
@@ -167,6 +167,7 @@ struct vec4
     vec4 &mul3(float f)       { x *= f; y *= f; z *= f; return *this; }
     vec4 &mul(float f)       { mul3(f); w *= f; return *this; }
     vec4 &add(const vec4 &o) { x += o.x; y += o.y; z += o.z; w += o.w; return *this; }
+    vec4 &sub(const vec4 &o) { x -= o.x; y -= o.y; z -= o.z; w -= o.w; return *this; }
     vec4 &neg3()             { x = -x; y = -y; z = -z; return *this; }
     vec4 &neg()              { neg3(); w = -w; return *this; }
 };
@@ -194,6 +195,7 @@ struct quat : vec4
     void restorew() { w = 1.0f-x*x-y*y-z*z; w = w<0 ? 0 : -sqrtf(w); }
     
     void add(const vec4 &o) { vec4::add(o); }
+    void sub(const vec4 &o) { vec4::sub(o); }
     void mul(float k) { vec4::mul(k); }
 
     void mul(const quat &p, const quat &o)
@@ -398,8 +400,16 @@ struct dualquat
 
     void accumulate(const dualquat &d, float k)
     {
-        real.add(vec4(d.real).mul(k));
-        dual.add(vec4(d.dual).mul(k));
+        if(real.dot(d.real) < 0)
+        {
+            real.sub(vec4(d.real).mul(k));
+            dual.sub(vec4(d.dual).mul(k));
+        }
+        else
+        {
+            real.add(vec4(d.real).mul(k));
+            dual.add(vec4(d.dual).mul(k));
+        }
     }
 
     vec transform(const vec &v) const
