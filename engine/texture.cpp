a6781e76d251ca5b9d9aa0e462e838d301053fb2
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2009-03-19 17:54:42 +0000

    added <mix> texture name command
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@714 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/texture.cpp
+++ engine/texture.cpp
@@ -188,6 +188,27 @@ void texdecal(ImageData &s)
     s.replace(m);
 }
 
+void texmix(ImageData &s, int c1, int c2, int c3, int c4)
+{
+    int numchans = c1 < 0 ? 0 : (c2 < 0 ? 1 : (c3 < 0 ? 2 : (c4 < 0 ? 3 : 4)));
+    if(numchans <= 0) return;
+    ImageData d(s.w, s.h, numchans);
+    uchar *dst = d.data, *src = s.data;
+    loopi(s.w*s.h)
+    {
+        switch(numchans)
+        {
+            case 4: dst[3] = src[c4];
+            case 3: dst[2] = src[c3];
+            case 2: dst[1] = src[c2];
+            case 1: dst[0] = src[c1];
+        }
+        src += s.bpp;
+        dst += d.bpp;
+    }
+    s.replace(d);
+}
+
 VAR(hwtexsize, 1, 0, 0);
 VAR(hwcubetexsize, 1, 0, 0);
 VAR(hwmaxaniso, 1, 0, 0);
@@ -656,13 +677,13 @@ static bool texturedata(ImageData &d, const char *tname, Slot::Tex *tex = NULL, 
     for(const char *pcmds = cmds; pcmds;)
     {
         #define PARSETEXCOMMANDS(cmds) \
-            const char *cmd = NULL, *end = NULL, *arg[3] = { NULL, NULL, NULL }; \
+            const char *cmd = NULL, *end = NULL, *arg[4] = { NULL, NULL, NULL, NULL }; \
             cmd = &cmds[1]; \
             end = strchr(cmd, '>'); \
             if(!end) break; \
             cmds = strchr(cmd, '<'); \
             size_t len = strcspn(cmd, ":,><"); \
-            loopi(3) \
+            loopi(4) \
             { \
                 arg[i] = strchr(i ? arg[i-1] : cmd, i ? ',' : ':'); \
                 if(!arg[i] || arg[i] >= end) arg[i] = ""; \
@@ -726,6 +747,7 @@ static bool texturedata(ImageData &d, const char *tname, Slot::Tex *tex = NULL, 
         else if(!strncmp(cmd, "offset", len)) texoffset(d, atoi(arg[0]), atoi(arg[1]));
         else if(!strncmp(cmd, "rotate", len)) texrotate(d, atoi(arg[0]), tex ? tex->type : 0);
         else if(!strncmp(cmd, "reorient", len)) texreorient(d, atoi(arg[0])>0, atoi(arg[1])>0, atoi(arg[2])>0, tex ? tex->type : TEX_DIFFUSE);
+        else if(!strncmp(cmd, "mix", len)) texmix(d, *arg[0] ? atoi(arg[0]) : -1, *arg[1] ? atoi(arg[1]) : -1, *arg[2] ? atoi(arg[2]) : -1, *arg[3] ? atoi(arg[3]) : -1);
         else if(!strncmp(cmd, "compress", len) || !strncmp(cmd, "dds", len)) 
         { 
             int scale = atoi(arg[0]);
