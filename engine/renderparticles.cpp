c146415155f20d2be760683e23e25733f1ce64a9
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2009-03-24 10:45:17 +0000

    set hex precision of particle colors to 3
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@844 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/renderparticles.cpp
+++ engine/renderparticles.cpp
@@ -1226,13 +1226,13 @@ bool printparticles(extentity &e, char *buf)
     switch(e.attr1)
     {
         case 0: case 4: case 7: case 8: case 9: case 10: case 11: case 12: 
-            s_sprintf(buf)("%s %d %d %d 0x%hX %d", entities::entname(e.type), e.attr1, e.attr2, e.attr3, e.attr4, e.attr5);
+            s_sprintf(buf)("%s %d %d %d 0x%.3hX %d", entities::entname(e.type), e.attr1, e.attr2, e.attr3, e.attr4, e.attr5);
             return true;
         case 3:
-            s_sprintf(buf)("%s %d %d 0x%hX %d %d", entities::entname(e.type), e.attr1, e.attr2, e.attr3, e.attr4, e.attr5);
+            s_sprintf(buf)("%s %d %d 0x%.3hX %d %d", entities::entname(e.type), e.attr1, e.attr2, e.attr3, e.attr4, e.attr5);
             return true;
         case 5: case 6:
-            s_sprintf(buf)("%s %d %d 0x%hX 0x%hX %d", entities::entname(e.type), e.attr1, e.attr2, e.attr3, e.attr4, e.attr5);
+            s_sprintf(buf)("%s %d %d 0x%.3hX 0x%.3hX %d", entities::entname(e.type), e.attr1, e.attr2, e.attr3, e.attr4, e.attr5);
             return true; 
     }
     return false;
