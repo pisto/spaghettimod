e673f8ebf90ccb629636778ff315a0937bbd6bd9
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2009-05-02 02:09:50 +0000

    playermodel menu
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@1669 07af1644-1164-4067-b897-63bf4dc9b748

--- fpsgame/render.cpp
+++ fpsgame/render.cpp
@@ -48,7 +48,7 @@ namespace game
     static const playermodelinfo playermodels[3] =
     {
         { "mrfixit", "mrfixit/blue", "mrfixit/red", "mrfixit/hudguns", NULL, "mrfixit/horns", { "mrfixit/armor/blue", "mrfixit/armor/green", "mrfixit/armor/yellow" }, "mrfixit", "mrfixit_blue", "mrfixit_red", true, true},
-        { "snoutx10k", "snoutx10k/blue", "snoutx10k/red", "snoutx10k/hudguns", NULL, "snoutx10k/wings", { "snoutx10k/armor/blue", "snoutx10k/armor/green", "snoutx10k/armor/yellow" }, "ironsnout", "ironsnout_blue", "ironsnout_red", true, true },
+        { "snoutx10k", "snoutx10k/blue", "snoutx10k/red", "snoutx10k/hudguns", NULL, "snoutx10k/wings", { "snoutx10k/armor/blue", "snoutx10k/armor/green", "snoutx10k/armor/yellow" }, "snoutx10k", "snoutx10k_blue", "snoutx10k_red", true, true },
         { "ogro/green", "ogro/blue", "ogro/red", "mrfixit/hudguns", "ogro/vwep", NULL, { NULL, NULL, NULL }, "ogro", "ogro", "ogro", false, false }
     };
 
