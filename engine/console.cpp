470ab4f0e9bb0bf85218c2847993fbd71f6c74f2
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2009-05-07 08:01:06 +0000

    removing stale blendbox code
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@1731 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/console.cpp
+++ engine/console.cpp
@@ -65,42 +65,6 @@ int rendercommand(int x, int y, int w)
     return height;
 }
 
-void blendbox(int x1, int y1, int x2, int y2, bool border)
-;
-#if 0
-{
-    notextureshader->set();
-
-    glDepthMask(GL_FALSE);
-    glDisable(GL_TEXTURE_2D);
-    glBlendFunc(GL_ZERO, GL_ONE_MINUS_SRC_COLOR);
-    glBegin(GL_QUADS);
-    if(border) glColor3d(0.5, 0.3, 0.4);
-    else glColor3d(1.0, 1.0, 1.0);
-    glVertex2f(x1, y1);
-    glVertex2f(x2, y1);
-    glVertex2f(x2, y2);
-    glVertex2f(x1, y2);
-    glEnd();
-    glDisable(GL_BLEND);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glBegin(GL_QUADS);
-    glColor3d(0.2, 0.7, 0.4);
-    glVertex2f(x1, y1);
-    glVertex2f(x2, y1);
-    glVertex2f(x2, y2);
-    glVertex2f(x1, y2);
-    glEnd();
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    xtraverts += 8;
-    glEnable(GL_BLEND);
-    glEnable(GL_TEXTURE_2D);
-    glDepthMask(GL_TRUE);
-
-    defaultshader->set();
-}
-#endif
-
 VARP(consize, 0, 5, 100);
 VARP(miniconsize, 0, 5, 100);
 VARP(miniconwidth, 0, 40, 100);
