6517ce3f7510ba0c684a5efc2adc2d2ac6995b31
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2009-11-03 20:09:54 +0000

    added Captain Cannon (work in progress)
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@1974 07af1644-1164-4067-b897-63bf4dc9b748

--- fpsgame/render.cpp
+++ fpsgame/render.cpp
@@ -8,7 +8,7 @@ namespace game
     VARP(ragdoll, 0, 1, 1);
     VARP(ragdollmillis, 0, 10000, 300000);
     VARP(ragdollfade, 0, 1000, 300000);
-    VARFP(playermodel, 0, 0, 3, { if(player1->clientnum < 0) player1->playermodel = playermodel; });
+    VARFP(playermodel, 0, 0, 4, { if(player1->clientnum < 0) player1->playermodel = playermodel; });
     VARP(forceplayermodels, 0, 0, 1);
 
     vector<fpsent *> ragdolls;
@@ -45,12 +45,13 @@ namespace game
         }
     }
 
-    static const playermodelinfo playermodels[4] =
+    static const playermodelinfo playermodels[5] =
     {
         { "mrfixit", "mrfixit/blue", "mrfixit/red", "mrfixit/hudguns", NULL, "mrfixit/horns", { "mrfixit/armor/blue", "mrfixit/armor/green", "mrfixit/armor/yellow" }, "mrfixit", "mrfixit_blue", "mrfixit_red", true, true},
         { "snoutx10k", "snoutx10k/blue", "snoutx10k/red", "snoutx10k/hudguns", NULL, "snoutx10k/wings", { "snoutx10k/armor/blue", "snoutx10k/armor/green", "snoutx10k/armor/yellow" }, "snoutx10k", "snoutx10k_blue", "snoutx10k_red", true, true },
         { "ogro/green", "ogro/blue", "ogro/red", "mrfixit/hudguns", "ogro/vwep", NULL, { NULL, NULL, NULL }, "ogro", "ogro_blue", "ogro_red", false, false },
-        { "inky", "inky/blue", "inky/red", "inky/hudguns", NULL, "inky/quad", { "inky/armor/blue", "inky/armor/green", "inky/armor/yellow" }, "inky", "inky_blue", "inky_red", true, true }
+        { "inky", "inky/blue", "inky/red", "inky/hudguns", NULL, "inky/quad", { "inky/armor/blue", "inky/armor/green", "inky/armor/yellow" }, "inky", "inky_blue", "inky_red", true, true },
+        { "captaincannon", "captaincannon/blue", "captaincannon/red", "captaincannon/hudguns", NULL, "captaincannon/quad", { "captaincannon/armor/blue", "captaincannon/armor/green", "captaincannon/armor/yellow" }, "captaincannon", "captaincannon_blue", "captaincannon_red", true, true }
     };
 
     int chooserandomplayermodel(int seed)
