1b48ab82b6aeee24bad861751eab7275a78792b9
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2010-02-11 02:55:20 +0000

    use nompedit in more blend ops
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@2292 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/blend.cpp
+++ engine/blend.cpp
@@ -529,9 +529,10 @@ COMMAND(setblendbrush, "s");
 COMMAND(getblendbrushname, "i");
 COMMAND(curblendbrush, "");
 
+extern int nompedit;
+
 bool canpaintblendmap(bool brush = true, bool sel = false, bool msg = true)
 {
-    extern int nompedit;
     if(noedit(!sel, msg) || (nompedit && multiplayer())) return false;
     if(!blendpaintmode)
     {
@@ -604,7 +605,7 @@ ICOMMAND(paintblendmap, "D", (int *isdown),
     
 void clearblendmapsel()
 {
-    if(noedit(false)) return;
+    if(noedit(false) || (nompedit && multiplayer())) return;
     extern selinfo sel;
     int x1 = sel.o.x>>BM_SCALE, y1 = sel.o.y>>BM_SCALE,
         x2 = (sel.o.x+sel.s.x*sel.grid+(1<<BM_SCALE)-1)>>BM_SCALE,
@@ -618,7 +619,7 @@ COMMAND(clearblendmapsel, "");
 
 void showblendmap()
 {
-    if(noedit(true)) return;
+    if(noedit(true) || (nompedit && multiplayer())) return;
     previewblends(ivec(0, 0, 0), ivec(worldsize, worldsize, worldsize));
 }
 
@@ -626,7 +627,7 @@ COMMAND(showblendmap, "");
 COMMAND(optimizeblendmap, "");
 ICOMMAND(clearblendmap, "", (),
 {
-    if(noedit(true)) return;
+    if(noedit(true) || (nompedit && multiplayer())) return;
     resetblendmap();
     showblendmap();
 });
