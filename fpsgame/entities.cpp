7c51724278b1f3bb281026afcd8e7a671fbbf470
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2010-03-24 21:03:31 +0000

    jumppadeffects tweak
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@2427 07af1644-1164-4067-b897-63bf4dc9b748

--- fpsgame/entities.cpp
+++ fpsgame/entities.cpp
@@ -240,9 +240,9 @@ namespace entities
             case JUMPPAD:
             {
                 if(d->lastpickup==ents[n]->type && lastmillis-d->lastpickupmillis<300) break;
-                jumppadeffects(d, n, true);
                 d->lastpickup = ents[n]->type;
                 d->lastpickupmillis = lastmillis;
+                jumppadeffects(d, n, true);
                 vec v((int)(char)ents[n]->attr3*10.0f, (int)(char)ents[n]->attr2*10.0f, ents[n]->attr1*12.5f);
                 if(d->ai) d->ai->becareful = true;
 				d->falling = vec(0, 0, 0);
