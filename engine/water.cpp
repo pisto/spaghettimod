25e55812282154d6cb9e6d2c77c2fcd5178b4ab0
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2010-04-19 05:33:18 +0000

    water vertex cleanups
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@2635 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/water.cpp
+++ engine/water.cpp
@@ -63,11 +63,11 @@ VERTWT(vertwtc, {
     varray::defattrib(varray::ATTRIB_COLOR, 4, GL_UNSIGNED_BYTE);
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
 }, {
-    varray::attrib<uchar>(0xFF, 0xFF, 0xFF, int(0x33 + fabs(s)*0x18));
+    varray::attrib<uchar>(wcol[0], wcol[1], wcol[2], int(0x33 + fabs(s)*0x18));
     varray::attrib<float>(v1+duv, v2+duv, v3+h);
 })
 VERTWN(vertwtcn, {
-    glColor4ub(0xFF, 0xFF, 0xFF, 0x33);
+    glColor4ub(wcol[0], wcol[1], wcol[2], 0x33);
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
 }, {
     varray::attrib<float>(v1, v2, v3+h);
@@ -77,12 +77,12 @@ VERTWT(vertwmtc, {
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
     varray::defattrib(varray::ATTRIB_TEXCOORD1, 3, GL_FLOAT);
 }, {
-    varray::attrib<uchar>(0xFF, 0xFF, 0xFF, int(0x33 + fabs(s)*0x18));
+    varray::attrib<uchar>(wcol[0], wcol[1], wcol[2], int(0x33 + fabs(s)*0x18));
     varray::attrib<float>(v1-duv, v2+duv, v3+h);
     varray::attrib<float>(v1+duv, v2+duv, v3+h);
 })
 VERTWN(vertwmtcn, {
-    glColor4ub(0xFF, 0xFF, 0xFF, 0x33);
+    glColor4ub(wcol[0], wcol[1], wcol[2], 0x33);
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
     varray::defattrib(varray::ATTRIB_TEXCOORD1, 3, GL_FLOAT);
 }, {
@@ -93,11 +93,11 @@ VERTWT(vertwetc, {
     varray::defattrib(varray::ATTRIB_COLOR, 4, GL_UNSIGNED_BYTE);
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
 }, {
-    varray::attrib<uchar>(0xFF, 0xFF, 0xFF, int(0x33 + fabs(s)*0x18));
+    varray::attrib<uchar>(wcol[0], wcol[1], wcol[2], int(0x33 + fabs(s)*0x18));
     varray::attrib<float>(v1+duv-camera1->o.x, v2+duv-camera1->o.y, camera1->o.z-(v3+h));
 })
 VERTWN(vertwetcn, {
-    glColor4ub(0xFF, 0xFF, 0xFF, 0x33);
+    glColor4ub(wcol[0], wcol[1], wcol[2], 0x33);
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
 }, {
     varray::attrib<float>(v1-camera1->o.x, v2-camera1->o.y, camera1->o.z-(v3+h));
@@ -107,12 +107,12 @@ VERTWT(vertwemtc, {
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
     varray::defattrib(varray::ATTRIB_TEXCOORD1, 3, GL_FLOAT);
 }, {
-    varray::attrib<uchar>(0xFF, 0xFF, 0xFF, int(0x33 + fabs(s)*0x18));
+    varray::attrib<uchar>(wcol[0], wcol[1], wcol[2], int(0x33 + fabs(s)*0x18));
     varray::attrib<float>(v1-duv, v2+duv, v3+h);
     varray::attrib<float>(v1+duv-camera1->o.x, v2+duv-camera1->o.y, camera1->o.z-(v3+h));
 })
 VERTWN(vertwemtcn, {
-    glColor4ub(0xFF, 0xFF, 0xFF, 0x33);
+    glColor4ub(wcol[0], wcol[1], wcol[2], 0x33);
     varray::defattrib(varray::ATTRIB_TEXCOORD0, 3, GL_FLOAT);
     varray::defattrib(varray::ATTRIB_TEXCOORD1, 3, GL_FLOAT);
 }, {
@@ -351,6 +351,8 @@ void setprojtexmatrix(Reflection &ref, bool init = true)
 
 void setuprefractTMUs()
 {
+    setuptmu(0, "= T");
+
     if(waterreflect || (waterenvmap && hasCM))
     { 
         glActiveTexture_(GL_TEXTURE1_ARB);
@@ -493,12 +495,10 @@ void renderwaterff()
                 float depth = !waterfog ? 1.0f : min(0.75f*m.depth/waterfog, 0.95f);
                 if(nowater || !waterrefract) depth = max(depth, nowater || (!waterreflect && (!waterenvmap || !hasCM)) || below ? 0.6f : 0.3f);
                 wcol[3] = int(depth*255);
-                if(!nowater && (waterrefract || ((waterreflect || (waterenvmap && hasCM)) && !below)))
+                if(!nowater && !waterrefract && ((waterreflect || (waterenvmap && hasCM)) && !below))
                 {
                     if(varray::data.length()) varray::end();
-                    float ec[4] = { wcol[0]/255.0f, wcol[1]/255.0f, wcol[2]/255.0f, depth };
-                    if(!waterrefract) { loopk(3) ec[k] *= depth; ec[3] = 1-ec[3]; }
-                    colortmu(0, ec[0], ec[1], ec[2], ec[3]);
+                    colortmu(0, depth*wcol[0]/255.0f, depth*wcol[1]/255.0f, depth*wcol[2]/255.0f, 1-depth);
                 }
                 lastdepth = m.depth;
             }
