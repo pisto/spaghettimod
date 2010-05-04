8451d93ab5fb48022e2a91f66237893a47743037
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2010-05-04 15:32:20 +0000

    default yaw to 0
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@2763 07af1644-1164-4067-b897-63bf4dc9b748

--- shared/ents.h
+++ shared/ents.h
@@ -72,7 +72,7 @@ struct physent                                  // base entity type, can be affe
 
     bool blocked;                               // used by physics to signal ai
 
-    physent() : o(0, 0, 0), deltapos(0, 0, 0), newpos(0, 0, 0), yaw(270), pitch(0), roll(0), maxspeed(100), 
+    physent() : o(0, 0, 0), deltapos(0, 0, 0), newpos(0, 0, 0), yaw(0), pitch(0), roll(0), maxspeed(100), 
                radius(4.1f), eyeheight(14), aboveeye(1), xradius(4.1f), yradius(4.1f), zmargin(0),
                state(CS_ALIVE), editstate(CS_ALIVE), type(ENT_PLAYER),
                collidetype(COLLIDE_ELLIPSE),
