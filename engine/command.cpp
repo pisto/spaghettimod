666c3fa7048d1b610771ff3a269a692a93b367c4
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2010-10-03 02:06:27 +0000

    lookup error tweaks
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@3261 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/command.cpp
+++ engine/command.cpp
@@ -1279,6 +1279,7 @@ static const uint *runcode(const uint *code, tagval &result)
                        arg.setstr(newstring(floatstr(*id->storage.f))));
             case CODE_LOOKUP|RET_STR:
                 id = identmap[op>>8];
+                if(id->valtype == VAL_NULL) conoutf(CON_ERROR, "unknown alias lookup: %s", id->name); 
                 args[numargs++].setstr(newstring(id->getstr()));
                 continue;
             case CODE_LOOKUPU|RET_INT:
@@ -1288,6 +1289,7 @@ static const uint *runcode(const uint *code, tagval &result)
                        arg.setint(int(*id->storage.f)));
             case CODE_LOOKUP|RET_INT:
                 id = identmap[op>>8];
+                if(id->valtype == VAL_NULL) conoutf(CON_ERROR, "unknown alias lookup: %s", id->name);   
                 args[numargs++].setint(id->getint());
                 continue;
             case CODE_LOOKUPU|RET_FLOAT:
@@ -1297,6 +1299,7 @@ static const uint *runcode(const uint *code, tagval &result)
                        arg.setfloat(*id->storage.f));
             case CODE_LOOKUP|RET_FLOAT:
                 id = identmap[op>>8];
+                if(id->valtype == VAL_NULL) conoutf(CON_ERROR, "unknown alias lookup: %s", id->name);   
                 args[numargs++].setfloat(id->getfloat());
                 continue;
             case CODE_LOOKUPU|RET_NULL:
@@ -1306,6 +1309,7 @@ static const uint *runcode(const uint *code, tagval &result)
                        arg.setfloat(*id->storage.f));
             case CODE_LOOKUP|RET_NULL:
                 id = identmap[op>>8];
+                if(id->valtype == VAL_NULL) conoutf(CON_ERROR, "unknown alias lookup: %s", id->name);   
                 id->getval(args[numargs++]);
                 continue;
 
