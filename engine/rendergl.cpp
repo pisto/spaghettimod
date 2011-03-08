7cc762391d8752b99cb6e121b998d16690bf8589
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2011-03-08 19:47:54 +0000

    hide non-critical mac warnings
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@3466 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/rendergl.cpp
+++ engine/rendergl.cpp
@@ -323,7 +323,7 @@ void gl_checkextensions()
 #if defined(__APPLE__) && SDL_BYTEORDER == SDL_BIG_ENDIAN
             if(strstr(vendor, "ATI") && (osversion<0x1050)) ati_oq_bug = 1;
 #endif
-            if(ati_oq_bug) conoutf(CON_WARN, "WARNING: Using ATI occlusion query bug workaround. (use \"/ati_oq_bug 0\" to disable if unnecessary)");
+            //if(ati_oq_bug) conoutf(CON_WARN, "WARNING: Using ATI occlusion query bug workaround. (use \"/ati_oq_bug 0\" to disable if unnecessary)");
         }
     }
     if(!hasOQ)
@@ -415,7 +415,7 @@ void gl_checkextensions()
         if(osversion>=0x1050) // fixed in 1055 for some hardware.. but not all..
         {
             apple_ff_bug = 1;
-            conoutf(CON_WARN, "WARNING: Using Leopard ARB_position_invariant bug workaround. (use \"/apple_ff_bug 0\" to disable if unnecessary)");
+            //conoutf(CON_WARN, "WARNING: Using Leopard ARB_position_invariant bug workaround. (use \"/apple_ff_bug 0\" to disable if unnecessary)");
         }
 #endif
     }
@@ -460,7 +460,7 @@ void gl_checkextensions()
             //if(osversion<0x1050) ??
             if(hasVP && hasFP) apple_glsldepth_bug = 1;
 #endif
-            if(apple_glsldepth_bug) conoutf(CON_WARN, "WARNING: Using Apple GLSL depth bug workaround. (use \"/apple_glsldepth_bug 0\" to disable if unnecessary");
+            //if(apple_glsldepth_bug) conoutf(CON_WARN, "WARNING: Using Apple GLSL depth bug workaround. (use \"/apple_glsldepth_bug 0\" to disable if unnecessary");
         }
     }
     
