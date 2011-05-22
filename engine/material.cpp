9995b78b6148621b81b12b6d6c47785403a586a7
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2011-05-22 06:01:02 +0000

    material load fix
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@3597 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/material.cpp
+++ engine/material.cpp
@@ -650,7 +650,7 @@ void rendermaterials()
             switch(m.material)
             {
                 case MAT_WATER:
-                    if(m.orient == O_TOP) continue;
+                    if(!wslot.loaded || m.orient == O_TOP) continue;
                     else if(lastmat==MAT_WATER) break;
                     else
                     {
@@ -747,6 +747,7 @@ void rendermaterials()
                     break;
 
                 case MAT_LAVA:
+                    if(!lslot.loaded) continue;
                     if(lastmat==MAT_LAVA && lastorient!=O_TOP && m.orient!=O_TOP) break;
                     else
                     {
