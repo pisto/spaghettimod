f49a2f9e0ef8b0829d549c601e56c6dda4d39a8f
rbrtpntn <rbrtpntn@07af1644-1164-4067-b897-63bf4dc9b748>
2011-08-03 04:45:12 +0000

    disable oq for intel gpus on osx
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@3645 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/rendergl.cpp
+++ engine/rendergl.cpp
@@ -314,6 +314,10 @@ void gl_checkextensions()
     }
     else conoutf(CON_WARN, "WARNING: No framebuffer object support. (reflective water may be slow)");
 
+#if defined(__APPLE__)
+    // Intel HD3000 broke occlusion queries - either causing software fallback, or returning wrong results
+    if(!strstr(vendor, "Intel"))
+#endif	   
     if(hasext(exts, "GL_ARB_occlusion_query"))
     {
         GLint bits;
