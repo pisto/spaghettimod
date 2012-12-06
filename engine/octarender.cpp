a34da32ccac7cb456a5f0b7c480ad4544c264281
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2012-12-06 12:24:18 +0000

    mergedface alpha fix
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4361 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/octarender.cpp
+++ engine/octarender.cpp
@@ -1331,8 +1331,8 @@ void updatevabbs(bool force)
 
 struct mergedface
 {   
-    uchar orient, mat, lmid, numverts;
-    ushort tex, envmap;
+    uchar orient, lmid, numverts;
+    ushort mat, tex, envmap;
     vertinfo *verts;
     int tjoints;
 };  
