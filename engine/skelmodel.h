4ea3d26d9274500fc5b7e36639f359e87aefe723
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2013-04-17 18:26:36 +0000

    warning fix
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4803 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/skelmodel.h
+++ engine/skelmodel.h
@@ -603,6 +603,8 @@ struct skelmodel : animmodel
     {
         int bone, target, parent;
         float pitchmin, pitchmax, pitchscale, pitchangle, pitchtotal;
+
+        pitchcorrect() : parent(-1), pitchangle(0), pitchtotal(0) {}
     };
 
     struct skeleton
