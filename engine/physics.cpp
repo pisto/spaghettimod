9dc55b823753f6ef8dfc64cbf5aa1e2eab35e2bf
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2013-08-08 18:51:36 +0000

    downoctree fix
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4873 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/physics.cpp
+++ engine/physics.cpp
@@ -377,7 +377,7 @@ float shadowray(ShadowRayCache *cache, const vec &o, const vec &ray, float radiu
     int side = O_BOTTOM, x = int(v.x), y = int(v.y), z = int(v.z);
     for(;;)
     {
-        DOWNOCTREE(shadowent, true);
+        DOWNOCTREE(shadowent, );
 
         cube &c = *lc;
         ivec lo(x&(~0<<lshift), y&(~0<<lshift), z&(~0<<lshift));
