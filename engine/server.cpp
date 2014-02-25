26cb662302b396cefff8865d06ebe6ac5250e1d8
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-02-25 08:08:36 +0000

    server info fix
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4952 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/server.cpp
+++ engine/server.cpp
@@ -561,7 +561,7 @@ void checkserversockets()        // reply all server info requests
         buf.data = pong;
         buf.dataLength = sizeof(pong);
         int len = enet_socket_receive(sock, &pongaddr, &buf, 1);
-        if(len < 0 || len > MAXPINGDATA) return;
+        if(len < 0 || len > MAXPINGDATA) continue;
         ucharbuf req(pong, len), p(pong, sizeof(pong));
         p.len += len;
         server::serverinforeply(req, p);
