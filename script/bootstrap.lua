4f3efd0c32785ac15a701a1cda291bd67c8675db
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-03 01:00:59 +0100

    remove intentional errors in bootstrap.lua

--- script/bootstrap.lua
+++ script/bootstrap.lua
@@ -3,7 +3,5 @@ function hooks.shuttingdown()
 	server.sendservmsg("\f6spaghetti finished :/")
 	engine.enet_host_flush(engine.serverhost)
 	print("bye")
-	error("asd")
 end
 
-error("dummy error to show a traceback, ignore me it's all fine")
