64476177d29bdfabd29b444add146a3e62fbbff5
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-03 21:25:46 +0100

    elaborate bootstrap.lua

--- script/bootstrap.lua
+++ script/bootstrap.lua
@@ -1,7 +1,23 @@
+--this table is not enforced but you shouldn't remove it
 hooks = {}
-function hooks.shuttingdown()
+
+--simple hook multiplexer
+local function makehookgroup()
+	local hookgroup = {}
+	return setmetatable(hookgroup, { __call = function(...)
+		for _, v in ipairs(hookgroup) do v(...) end
+	end})
+end
+rawset(spaghetti, "makehookgroup", makehookgroup)
+
+
+
+hooks.shuttingdown = spaghetti.makehookgroup()
+
+table.insert(hooks.shuttingdown, function()
 	server.sendservmsg("\f6spaghetti finished :/")
 	engine.enet_host_flush(engine.serverhost)
-	print("bye")
-end
-
+	--if the bye message and disconnect packet are received at the same time, the former is not processed
+	os.execute("sleep .1")
+  print("bye")
+end)
