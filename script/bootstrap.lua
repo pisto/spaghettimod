e9d6f1839b23a80a6160f5ee6d6101e7081c2cc4
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-11 12:31:25 +0100

    use engine.writelog instead of print

--- script/bootstrap.lua
+++ script/bootstrap.lua
@@ -19,7 +19,7 @@ table.insert(hooks.tick, function()
   local now = math.ceil(engine.totalmillis/1000)
   if now <= lastsecond then return end
   lastsecond = now
-  print(lastsecond % 2 == 1 and "tick!" or "tock!")
+  engine.writelog(lastsecond % 2 == 1 and "tick!" or "tock!")
 end)
 
 hooks.shuttingdown = spaghetti.makehookgroup()
@@ -29,7 +29,7 @@ table.insert(hooks.shuttingdown, function(info)
   engine.enet_host_flush(engine.serverhost)
   --if the bye message and disconnect packet are received at the same time, the former is not processed
   os.execute("sleep .1")
-  print("bye")
+  engine.writelog("bye")
 end)
 
 
@@ -37,13 +37,13 @@ hooks[20] = spaghetti.makehookgroup() --N_GUNSELECT
 table.insert(hooks[20], function(p)
   local selects, gun = p.cq.extra.gunselects or {}, p.gunselect
   selects[gun] = (selects[gun] or 0) + 1
-  print(string.format("cn %d selected weapon %d %d times", p.cq.clientnum, gun, selects[gun]))
+  engine.writelog(string.format("cn %d selected weapon %d %d times", p.cq.clientnum, gun, selects[gun]))
   p.cq.extra.gunselects = selects
 end)
 
 hooks[86] = spaghetti.makehookgroup() --N_AUTHTRY
 table.insert(hooks[86], function(p)
-  print(string.format("cn %d tried auth (%s) as %s. Blocking it.", p.ci.clientnum, p.desc == "" and "<global>" or p.desc, p.name))
+  engine.writelog(string.format("cn %d tried auth (%s) as %s. Blocking it.", p.ci.clientnum, p.desc == "" and "<global>" or p.desc, p.name))
   p.skip = true
 end)
 
