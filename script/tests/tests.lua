479e9617066c89d15c4c4817e4282361656d940e
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-13 23:05:18 +0100

    less hacky test script, showcase packet forging

--- script/tests/tests.lua
+++ script/tests/tests.lua
@@ -10,6 +10,7 @@ spaghetti.addhook("shuttingdown", function(info)
   if info.servererror then return end
   server.sendservmsg("\f6spaghetti finished :/")
   engine.flushserver(true)
+  engine.enet_host_flush(engine.serverhost)
   --trick to make the master server delete us in 15 seconds instead of one hour
   engine.requestmaster(string.format("regserv %d\n", cs.serverport))
   engine.flushmasteroutput()
@@ -19,13 +20,19 @@ spaghetti.addhook("shuttingdown", function(info)
 end)
 
 spaghetti.addhook(server.N_GUNSELECT, function(p)
-  local selects, gun = p.cq.extra.gunselects or {}, p.gunselect
+  local cq = p.cq
+  if not cq then return end
+  local selects, gun = cq.extra.gunselects or {}, p.gunselect
   selects[gun] = (selects[gun] or 0) + 1
-  engine.writelog(string.format("cn %d selected weapon %d %d times", p.cq.clientnum, gun, selects[gun]))
-  p.cq.extra.gunselects = selects
+  engine.writelog(string.format("%s(%d) selected weapon %d %d times", cq.name, cq.clientnum, gun, selects[gun]))
+  cq.extra.gunselects = selects
 end)
 
 spaghetti.addhook(server.N_AUTHTRY, function(p)
-  engine.writelog(string.format("cn %d tried auth (%s) as %s. Blocking it.", p.ci.clientnum, p.desc == "" and "<global>" or p.desc, p.name))
+  local ci = p.ci
+  engine.writelog(string.format("%s(%d) tried auth (%s) as %s. Blocking it.", ci.name, ci.clientnum, p.desc == "" and "<global>" or p.desc, p.name))
+  local msg = engine.packetbuf(136, engine.ENET_PACKET_FLAG_RELIABLE)
+  msg:putint(server.N_SERVMSG):sendstring("no auth for you!")
+  engine.sendpacket(ci.clientnum, 1, msg:finalize(), -1)
   p.skip = true
 end)
