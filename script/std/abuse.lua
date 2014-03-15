2155a2ddcf026aaad713b352f5ed178d428033d7
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-16 00:23:48 +0100

    packet rate limiting

--- script/std/abuse.lua
+++ script/std/abuse.lua
@@ -23,4 +23,37 @@ local function blockmasterkick(value)
 end
 
 
-return {blockmasterkick = blockmasterkick}
+
+--packet rate limit
+
+local fp, lambda, tb = require"utils.fp", require"utils.lambda", require"utils.tokenbucket"
+local noop, I, map, L, U = fp.noop, fp.I, fp.map, lambda.L, fp.U
+
+local function ratelimit(packets, rate, maxtokens, selector)
+  if type(packets) == "number" then packets = {packets} end
+  packets = setmetatable({U(packets)}, tokentag)
+  packets.hooks = map.m(function(_, p)
+    return p, spaghetti.addhook(p, function(info)
+      local skip, desc, client
+      if selector then
+        skip, desc, client = selector(info)
+        if skip then return end
+      end
+      local extra = (client or info.ci).extra
+      extra.ratelimit = extra.ratelimit or {}
+      extra.ratelimit[packets] = extra.ratelimit[packets] or tb(rate, maxtokens)
+      if extra.ratelimit[packets]() then return end
+      info.skip, info.ratelimited = true, (desc or true)
+
+    end, true) end
+  , packets)
+  return packets
+end
+
+local function delratelimit(token)
+  map.pn(L"spaghetti.removehook(...)", token.hooks)
+end
+
+
+
+return { blockmasterkick = blockmasterkick, ratelimit = ratelimit, delratelimit = delratelimit }
