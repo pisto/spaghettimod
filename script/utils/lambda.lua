8f0ca764350d1175b65ee5d1982fa9b7cc224e82
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-15 22:52:30 +0100

    throw an error when the lambda cannot be compiled

--- script/utils/lambda.lua
+++ script/utils/lambda.lua
@@ -1,9 +1,10 @@
 local compiled = {}
 
 local function L(code)
-	local cached = compiled[code]
+	local cached, err = compiled[code]
 	if not cached then
-		cached = loadstring("local _1, _2, _3, _4, _5, _6, _7, _8, _9 = ... local _ = _1 " .. code, "<lambda>")
+		cached, err = loadstring("local _1, _2, _3, _4, _5, _6, _7, _8, _9 = ... local _ = _1 " .. code, "<lambda>")
+		if not cached then error(err) end
 		compiled[code] = cached
 	end
 	return cached
