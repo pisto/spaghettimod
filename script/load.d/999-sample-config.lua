4ea0b09a6ab2e5393ebf03847cff22f7d895fd21
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-15 12:50:14 +0100

    add a configuration from my server (off by default)

---
+++ script/load.d/999-sample-config.lua
@@ -0,0 +1,41 @@
+--[[
+
+  This is the configuration I use on my server.
+
+]]--
+
+if not os.getenv("PISTOVPS") then engine.writelog("Not applying the sample configuration.") return end
+engine.writelog("Applying the sample configuration.")
+
+--make sure you delete the next two lines, or I'll have admin on your server.
+cs.serverauth = "pisto"
+cs.adduser("pisto", "pisto", "+8ce1687301aea5c4500df0042849191f875c70555c3cc4c9", "a")
+
+local logdate = require"std.logdate"
+logdate.on = true
+
+cs.serverdesc = "\f7github:\f2spaghettimod"
+local trap = require"tests.light-trap"
+trap.set(0.2, "github.com/pisto/spaghettimod   ")
+
+cs.lockmaprotation = 2
+cs.maprotationreset()
+--copied from data/menus.cfg
+local ffamaps, capturemaps = table.concat({
+  "aard3c academy akaritori alithia alloy aqueducts arbana bvdm_01 castle_trap collusion complex corruption curvedm curvy_castle darkdeath deathtek depot",
+  "dirtndust DM_BS1 dock douze duel7 duel8 dune elegy fanatic_quake force fragplaza frostbyte frozen fury guacamole gubo hades",
+"hashi hog2 industry injustice island justice kalking1 katrez_d kffa killfactory kmap5 konkuri-to ksauer1 legazzo lostinspace masdm mbt10",
+  "mbt2 mbt9 memento metl2 metl3 metl4 moonlite neondevastation neonpanic nmp8 nucleus oasis oddworld ogrosupply orbe orion osiris",
+  "ot outpost paradigm park pgdm phosgene pitch_black powerplant refuge renegade rm5 roughinery ruby ruine sauerstruck sdm1 shadowed",
+  "shindou shinmei1 shiva simplicity skrdm1 stemple suburb tartech teahupoo tejen thetowers thor torment tumwalk turbine wake5 wdcd"
+}, " "), table.concat({
+  "abbey akroseum alithia arabic asgard asteroids c_egypt c_valley campo capture_night caribbean collusion core_refuge core_transfer corruption cwcastle damnation",
+  "dirtndust donya duomo dust2 eternal_valley evilness face-capture fb_capture fc3 fc4 fc5 forge frostbyte hades hallo haste hidden",
+  "infamy killcore3 kopenhagen lostinspace mbt12 mercury monastery nevil_c nitro nmp4 nmp8 nmp9 nucleus ogrosupply paradigm ph-capture reissen",
+  "relic river_c serenity snapper_rocks spcr subterra suburb tempest tortuga turbulence twinforts urban_c valhalla venice xenon"
+}, " ")
+
+cs.maprotation("?ffa ?effic ?tac", ffamaps, "?regencapture ?capture", capturemaps)
+cs.publicserver = 2
+local abuse = require"std.abuse"
+abuse.blockmasterkick("no.")
