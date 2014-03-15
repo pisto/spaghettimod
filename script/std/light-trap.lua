cf87b8d7be7af8fb528ceb6757e33c431f5781a0
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-15 14:07:14 +0100

    make light-trap fully disposable

--- script/std/light-trap.lua
+++ script/std/light-trap.lua
@@ -9,59 +9,64 @@ local fp = require"utils.fp"
 local map, range = fp.map, fp.range
 
 
-local pingers
-
-spaghetti.addhook("ping", function(info)
-  if not pingers then return end
-  local req, p = info.req, info.p
-  local clientmillis = req:getint()
-  if clientmillis == 0 then req.len = 0 return end
-  local pinger = pingers[engine.pongaddr.host] or {}
-  pinger.lastping = engine.totalmillis
-  pinger.millisoffset = clientmillis - engine.totalmillis
-  pinger.port = engine.pongaddr.port
-  pingers[engine.pongaddr.host] = pinger
-  info.skip = true
-end)
-
-
-local interval, lastcheck, desctable, currentpos
+local state
 
 local function set(f, wow)
-  if not f or f == 0 then pingers, interval, lastcheck, desctable, currentpos = nil return end
-  pingers = pingers or {}
-  local oldlen = desctable and #desctable
+  if not f or f == 0 then
+    if not state then return end
+    spaghetti.removehook("ping", state.pinghook)
+    spaghetti.removehook("tick", state.tickhook)
+    state = nil
+  end
+
+  state = state or { pingers = {} }
+  local oldlen = state.descs and #state.descs
   local concat = wow:rep(math.ceil(25/#wow) + 1)
-  desctable = map.f(function(i)
+  state.descs = map.f(function(i)
     return concat:sub(i, i + 24)
   end, range.z(1, #wow))
-  if oldlen ~= #wow then currentpos = 1 end
+  if oldlen ~= #wow then state.nextdesc = 1 end
   local newinterval = 1000 / (f*#wow)
-  if interval ~= newinterval then
-    interval = newinterval
-    lastcheck = -1000
+  if state.interval ~= newinterval then
+    state.interval = newinterval
+    state.lastcheck = -1000
   end
+
+  if not state.pinghook then
+    local fakereq, fakep = engine.packetbuf(1, 0), engine.packetbuf(5000, 0)
+    fakereq:putint(1)
+
+    state.pinghook = spaghetti.addhook("ping", function(info)
+      local req, p = info.req, info.p
+      local clientmillis = req:getint()
+      if clientmillis == 0 then req.len = 0 return end
+      local pinger = state.pingers[engine.pongaddr.host] or {}
+      pinger.lastping = engine.totalmillis
+      pinger.millisoffset = clientmillis - engine.totalmillis
+      pinger.port = engine.pongaddr.port
+      state.pingers[engine.pongaddr.host] = pinger
+      info.skip = true
+    end)
+
+    state.tickhook = spaghetti.addhook("tick", function()
+      if engine.totalmillis - state.lastcheck < state.interval then return end
+      state.lastcheck = engine.totalmillis
+      local origdesc, orighost, origport = cs.serverdesc, engine.pongaddr.host, engine.pongaddr.port
+      cs.serverdesc = state.descs[state.nextdesc]
+      map.pn(function(ip, pinger)
+        if state.lastcheck - pinger.lastping > 10000 then state.pingers[ip] = nil return end
+        fakereq.len, fakep.len = 0, 0
+        fakep:putint(pinger.millisoffset + state.lastcheck)
+        engine.pongaddr.host, engine.pongaddr.port = ip, pinger.port
+        server.serverinforeply(fakereq, fakep)
+      end, state.pingers)
+      cs.serverdesc = origdesc
+      state.nextdesc = state.nextdesc % #state.descs + 1
+    end)
+
+  end
+
 end
-
-
-local fakereq, fakep = engine.packetbuf(1, 0), engine.packetbuf(5000, 0)
-fakereq:putint(1)
-
-spaghetti.addhook("tick", function()
-  if not interval or engine.totalmillis - lastcheck < interval then return end
-  lastcheck = engine.totalmillis
-  local origdesc, orighost, origport = cs.serverdesc, engine.pongaddr.host, engine.pongaddr.port
-  cs.serverdesc = desctable[currentpos]
-  map.pn(function(ip, pinger)
-    if lastcheck - pinger.lastping > 10000 then pingers[ip] = nil return end
-    fakereq.len, fakep.len = 0, 0
-    fakep:putint(pinger.millisoffset + lastcheck)
-    engine.pongaddr.host, engine.pongaddr.port = ip, pinger.port
-    server.serverinforeply(fakereq, fakep)
-  end, pingers)
-  cs.serverdesc = origdesc
-  currentpos = currentpos % #desctable + 1
-end)
 
 
 return {set = set}
