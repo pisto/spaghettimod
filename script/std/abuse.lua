8e8c029e3f485e8ed363e342585386b5e87d436d
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-15 02:41:29 +0100

    proper deinitialization in blockmasterkick

--- script/std/abuse.lua
+++ script/std/abuse.lua
@@ -4,19 +4,27 @@
 
 ]]--
 
-local module = { blockmasterkick = false }
 
-spaghetti.addhook(server.N_KICK, function(info)
-  if not module.blockmasterkick or info.ci.privilege > server.PRIV_MASTER then return end
-  info.skip = true
-  engine.sendpacket(info.ci.clientnum, 1,
-    engine.packetbuf(30, engine.ENET_PACKET_FLAG_RELIABLE)
-    :putint(server.N_SERVMSG)
-    :sendstring(type(module.blockmasterkick) == "string" and module.blockmasterkick or "You need a higher privilege to kick.")
-    :finalize()
-  , -1)
-end)
+--block public masters from kicking
+local kicktoken, blockmastervalue
+local function blockmasterkick(value)
+  if value and not kicktoken then
+    kicktoken = spaghetti.addhook(server.N_KICK, function(info)
+      if info.skip or info.ci.privilege > server.PRIV_MASTER then return end
+      info.skip = true
+      engine.sendpacket(info.ci.clientnum, 1,
+        engine.packetbuf(30, engine.ENET_PACKET_FLAG_RELIABLE)
+          :putint(server.N_SERVMSG)
+          :sendstring(type(blockmastervalue) == "string" and blockmastervalue or "You need a higher privilege to kick.")
+          :finalize()
+      , -1)
+    end)
+  elseif not value and kicktoken then
+    spaghetti.removehook(server.N_KICK, kicktoken)
+    kicktoken = nil
+  end
+  blockmastervalue = value
+end
 
 
-return module
-
+return {blockmasterkick = blockmasterkick}
