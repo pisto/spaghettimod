91a830d6ab13e14233edea840372ab7842d8cf50
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-20 18:10:59 +0100

    find clients by id

--- script/std/uuid.lua
+++ script/std/uuid.lua
@@ -5,13 +5,18 @@
 ]]--
 
 local uuid, fp, lambda = require"uuid", require"utils.fp", require"utils.lambda"
-local fold, last, Lr = fp.fold, fp.last, lambda.Lr
+local fold, first, last, pick, Lr = fp.fold, fp.first, fp.last, fp.pick, lambda.Lr
 
 local urandom = io.open("/dev/urandom")
 uuid.randomseed(last(fold.vz(Lr"_1 * 256 + _2", 0, urandom:read(7):byte())))
 urandom:close()
 
-local function adduuid(info) info.ci.extra.uuid = uuid() end
+local uuids = {}
+
+local function adduuid(info)
+  info.ci.extra.uuid = uuid()
+  uuids[info.ci.extra.uuid] = info.ci
+end
 
 spaghetti.addhook("clientconnect", adduuid)
 spaghetti.addhook("botjoin", adduuid)
@@ -19,3 +24,12 @@ spaghetti.addhook("botjoin", adduuid)
 for ci in require("std.iterators").all() do
   if not ci.extra.uuid then ci.extra.uuid = uuid() end
 end
+
+local iterators = require"std.iterators"
+local function find(uuid)
+  return first(pick.zf(function(ci)
+    return ci.extra.uuid == uuid
+  end, iterators.all()))
+end
+
+return { find = find }
