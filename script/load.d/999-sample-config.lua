095811455a2c37008122fc36993c918be7ed3792
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-20 19:14:37 +0100

    fix it and don't protect the call.

--- script/load.d/999-sample-config.lua
+++ script/load.d/999-sample-config.lua
@@ -138,18 +138,15 @@ spaghetti.addhook("changemap", L"engine.writelog(string.format('new %s on %s', s
 
 
 --geoip
-local ok, error = pcall(function()
-  local geoip, geoipcity = require"geoip", require"geoip.city"
-  local db = geoipcity.open("/usr/share/GeoIP/GeoLiteCity.dat", geoip.MEMORY_CACHE)
-  db:set_charset(geoip.UTF8)
-  spaghetti.addhook("connected", function(info)
-    local record = db:query_by_ipnum(engine.ENET_NET_TO_HOST_32(engine.getclientip(info.ci.clientnum)))
-    if not record or not record.country_name then return end
-    if record.city then
-      engine.servermsg(string.format("%s(%c) connects from %s in %s", info.ci.name, info.ci.clientnum, engine.decodeutf8(record.city), engine.decodeutf8(record.country_name)))
-    else
-      engine.servermsg(string.format("%s(%c) connects from %s", info.ci.name, info.ci.clientnum, engine.decodeutf8(record.country_name)))
-    end
-  end)
+local geoip, geoipcity = require"geoip", require"geoip.city"
+local db = geoipcity.open("/usr/share/GeoIP/GeoLiteCity.dat", geoip.MEMORY_CACHE)
+db:set_charset(geoip.UTF8)
+spaghetti.addhook("connected", function(info)
+  local record = db:query_by_ipnum(engine.ENET_NET_TO_HOST_32(engine.getclientip(info.ci.clientnum)))
+  if not record or not record.country_name then return end
+  if record.city then
+    server.sendservmsg(string.format("%s(%d) connects from %s in %s", info.ci.name, info.ci.clientnum, engine.decodeutf8(record.city), engine.decodeutf8(record.country_name)))
+  else
+    server.sendservmsg(string.format("%s(%d) connects from %s", info.ci.name, info.ci.clientnum, engine.decodeutf8(record.country_name)))
+  end
 end)
-if not ok then engine.writelog("Cannot load geoip support: " .. error) end
