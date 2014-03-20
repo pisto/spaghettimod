38cc076503a3e6bf37f722938331b97fb49ab3d0
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-20 16:56:16 +0100

    utils.later fixes, add later based on gamemillis

--- script/utils/later.lua
+++ script/utils/later.lua
@@ -6,34 +6,42 @@
 
 local module = {}
 
-local lambda = require"utils.lambda"
+local lambda, fp = require"utils.lambda", require"utils.fp"
 local Lr = lambda.Lr
 
-local futures, tickhooktoken = {}
+local queues = { abs = {}, game = {} }
 
-function module.later(delay, lambda, rep)
-  local token = { when = delay + engine.totalmillis, lambda = lambda }
-  if rep then token.delay = delay end
+local function later(delay, lambda, rep, futures, timer)
+  local token = { when = delay + timer(), lambda = lambda, delay = rep and delay, futures = futures }
   table.sort(table.insert(futures, token), Lr"_1.when < _2.when")
-  if not tickhooktoken then
-    tickhooktoken = spaghetti.addhook("tick", function()
-      while futures[1] and futures[1].when <= engine.totalmillis do
+  if not futures.tickhooktoken then
+    futures.tickhooktoken = spaghetti.addhook("tick", function()
+      while futures[1] and futures[1].when <= timer() do
         local f = table.remove(futures, 1)
         local ok, msg = xpcall(f.lambda, spaghetti.stackdumper)
-        if not ok then engine.writelog("One future of resulted in an error" .. (r.delay and " and has been cancelled" or "") .. ": " .. msg)
+        if not ok then engine.writelog("One future of resulted in an error" .. (f.delay and " and has been cancelled" or "") .. ": " .. msg)
         elseif f.delay then
           f.when = f.when + f.delay
           table.sort(table.insert(futures, token), Lr"_1.when < _2.when")
         end
       end
-      if #futures == 0 then spaghetti.removehook("tick", tickhooktoken) end
+      if #futures == 0 then spaghetti.removehook("tick", futures.tickhooktoken) end
     end)
   end
   return token
 end
 
 function module.cancel(token)
-  for i, v in futures do if v == token then table.remove(futures, i) return end end
+  for i, v in ipairs(token.futures) do if v == token then table.remove(token.futures, i) return end end
+end
+
+function module.later(delay, lambda, rep)
+  return later(delay, lambda, rep, queues.abs, Lr"engine.totalmillis")
+end
+
+function module.latergame(delay, lambda, rep)
+  local token = later(delay, lambda, rep, queues.game, Lr"server.gamemillis")
+  spaghetti.addhook("changemap", function() module.cancel(token) end)
 end
 
 return module
