ec96b6844286185ef7d397bcc3ce52dfe6764bc0
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-20 15:28:52 +0100

    config updates

--- script/load.d/999-sample-config.lua
+++ script/load.d/999-sample-config.lua
@@ -85,7 +85,7 @@ map.nv(function(type) spaghetti.addhook(type, warnspam) end,
 --force the client back to his original name if his N_SWITCHNAME packet has been ratelimited
 spaghetti.addhook(server.N_SWITCHNAME, function(info)
   if not info.ratelimited then return end
-  local newname = engine.filtertext(info.text):sub(1, server.MAXNAMELEN)
+  local newname = engine.filtertext(info.text):sub(1, server.MAXNAMELEN):gsub("^$", "unnamed")
   if newname == info.ci.name then return end
   local rename = engine.packetbuf(1 + #info.ci.name, engine.ENET_PACKET_FLAG_RELIABLE):putint(server.N_SWITCHNAME):sendstring(info.ci.name)
   rename = n_client(rename, info.ci)
@@ -96,4 +96,38 @@ end)
 spaghetti.addhook(server.N_TEXT, function(info)
   if info.skip or (info.text ~= "#help" and info.text ~= "#version" and info.text ~= "#info") then return end
   playermsg("spaghettimod is a reboot of hopmod for programmers. Will be used for SDoS.\nKindly brought to you by pisto.", info.ci)
+end)
+
+--track players better in the log: more informative connect/disconnect messages, track name changes
+spaghetti.addhook("log", function(info)
+  if info.s:match"^client connected" or info.s:match"^client [^ ]+ disconnected" or info.s:match"^disconnected client" then info.skip = true return end   --will make a better message in the clientdisconnect hook
+end, true)
+
+local ip = require"utils.ip"
+local function conninfo(client)
+  local name, cn= "", client
+  if type(client) ~= 'number' then name, cn = client.name, client.clientnum end
+  local peer = engine.getclientpeer(cn)
+  return string.format('%s(%d) %s:%d:%d', name, cn, tostring(ip.ip(peer.address.host)), peer.address.port, peer.incomingPeerID)
+end
+spaghetti.addhook("clientconnect", function(info)
+  engine.writelog("connect: " .. conninfo(info.ci))
+end)
+spaghetti.addhook("connected", function(info)
+  engine.writelog("join: " .. conninfo(info.ci))
+end)
+spaghetti.addhook(server.N_SWITCHNAME, function(info)
+  if info.skip then return end
+  engine.writelog(string.format('rename: %s -> %s(%d)', conninfo(info.ci), engine.filtertext(info.text):sub(1, server.MAXNAMELEN):gsub("^$", "unnamed"), info.ci.clientnum))
+end)
+spaghetti.addhook("clientdisconnect", function(info)
+  engine.writelog(string.format("disconnecting: %s %s", conninfo(info.ci), engine.disconnectreason(info.reason) or "none"))
+end)
+spaghetti.addhook("enetevent", function(info)
+  if info.skip or info.event.type ~= engine.ENET_EVENT_TYPE_DISCONNECT then return end
+  if info.ci then engine.writelog("disconnected: " .. conninfo(info.ci))
+  else
+    local peer = info.event.peer
+    engine.writelog(string.format('martian %s:%d:%d', tostring(ip.ip(peer.address.host)), peer.address.port, peer.incomingclientID))
+  end
 end)
