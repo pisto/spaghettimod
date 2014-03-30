4962747309c0f4a35be1ce80ac52a643cb6a1d4c
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-03-30 16:11:18 +0200

    record demos on my server

---
+++ script/load.d/400-demorecord.lua
@@ -0,0 +1,35 @@
+--[[
+
+  Demo record, flushed every second.
+
+]]--
+
+local recordingdemo
+
+spaghetti.addhook("enddemorecord", function()
+  engine.writelog("stopped recording " .. recordingdemo)
+  recordingdemo = nil
+end)
+
+spaghetti.addhook("setupdemorecord", function(info)
+  recordingdemo = "demos/" .. os.date("%c - " .. server.modename(server.gamemode, '?') .. " - " .. server.smapname:gsub("[%c%p%s]", "_") .. ".dmo")
+  info.filename = recordingdemo
+  engine.writelog("started recording " .. recordingdemo)
+end)
+
+local lastflush = -1000
+spaghetti.addhook("recordpacket", function(info)
+  if engine.totalmillis - lastflush > 1000 then lastflush, info.flush = engine.totalmillis, true end
+end)
+
+local laststep = server.shouldstep
+spaghetti.addhook("tick", function()
+  if not recordingdemo or server.shouldstep == laststep then return end
+  laststep = server.shouldstep
+  local notice = engine.packetbuf(40, engine.ENET_PACKET_FLAG_RELIABLE)
+  notice:putint(server.N_SERVMSG):sendstring(os.date("%c | " .. (laststep and "resumed" or "stopped")))
+  server.recordpacket(1, notice.buf:sub(1, notice.len))
+end)
+
+spaghetti.addhook("changemap", function() return recordingdemo or server.setupdemorecord() end)
+spaghetti.addhook("shuttingdown", function() server.enddemorecord() end)
