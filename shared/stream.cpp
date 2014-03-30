cbdf867c099c1d20113bba5718972b6f6d0aa422
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-03-30 21:12:47 +0000

    flushbuf fix
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4961 07af1644-1164-4067-b897-63bf4dc9b748

--- shared/stream.cpp
+++ shared/stream.cpp
@@ -946,11 +946,12 @@ struct gzstream : stream
         return len - zfile.avail_out;
     }
 
-    bool flushbuf()
+    bool flushbuf(bool full = false)
     {
+        if(full) deflate(&zfile, Z_SYNC_FLUSH);
         if(zfile.next_out && zfile.avail_out < BUFSIZE)
         {
-            if(file->write(buf, BUFSIZE - zfile.avail_out) != int(BUFSIZE - zfile.avail_out) || !file->flush())
+            if(file->write(buf, BUFSIZE - zfile.avail_out) != int(BUFSIZE - zfile.avail_out) || (full && !file->flush()))
                 return false;
         }
         zfile.next_out = buf;
@@ -958,11 +959,7 @@ struct gzstream : stream
         return true;
     }
 
-    bool flush()
-    {
-        deflate(&zfile, Z_SYNC_FLUSH);
-        return flushbuf();
-    }
+    bool flush() { return flushbuf(true); }
 
     int write(const void *buf, int len)
     {
@@ -971,7 +968,7 @@ struct gzstream : stream
         zfile.avail_in = len;
         while(zfile.avail_in > 0)
         {
-            if(!zfile.avail_out && !flush()) { stopwriting(); break; }
+            if(!zfile.avail_out && !flushbuf()) { stopwriting(); break; }
             int err = deflate(&zfile, Z_NO_FLUSH);
             if(err != Z_OK) { stopwriting(); break; }
         }
