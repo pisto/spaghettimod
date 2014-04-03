6a8649ae468928e038713760bdf70394e7b1f458
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-04-03 22:26:40 +0200

    fix master status in scoreboard when exiting limbo

--- script/std/limbo.lua
+++ script/std/limbo.lua
@@ -7,16 +7,16 @@
 local module = {}
 
 local iterators, fp, lambda = require"std.iterators", require"utils.fp", require"utils.lambda"
-local map, L, Lr = fp.map, lambda.L, lambda.Lr
+local map, I, L, Lr = fp.map, fp.I, lambda.L, lambda.Lr
 
-local putf = require"std.putf"
+local putf, putf, iterators = require"std.putf", require"std.putf", require"std.iterators"
 
-local connecttoken, timeouttoken, martiantoken
+local connecttoken, timeouttoken, martiantoken, connectedtoken
 function module.on(on)
 
   if not on and connecttoken then
-    map.nv(spaghetti.removehook, connecttoken, timeouttoken, martiantoken)
-    connecttoken, timeouttoken, martiantoken = nil
+    map.nv(spaghetti.removehook, connecttoken, timeouttoken, martiantoken, connectedtoken)
+    connecttoken, timeouttoken, martiantoken, connectedtoken = nil
     map.nf(Lr"_.extra.limbo and _.extra.limbo.release()", iterators.all())
 
   elseif on and not connecttoken then
@@ -39,7 +39,11 @@ function module.on(on)
       info.skip = true
       if spaghetti.hooks.enterlimbo then spaghetti.hooks.enterlimbo{ ci = info.ci } end
     end)
-
+    connectedtoken = spaghetti.addhook("connected", function(info)
+      local p = putf({ 30, engine.ENET_PACKET_FLAG_RELIABLE }, server.N_CURRENTMASTER, server.mastermode)
+      map.nf(function(ci) putf(p, ci.clientnum, ci.privilege) end, iterators.minpriv(server.PRIV_MASTER))
+      engine.sendpacket(-1, 1, p:putint(-1):finalize(), -1)
+    end)
     timeouttoken, martiantoken = spaghetti.addhook("jointimeout", L"_.skip = true"), spaghetti.addhook("martian", L"if not _.ci.connected then _.skip = true end")
 
   end
