84260b05998759f7074886315110cb5da2c81276
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-04-03 17:17:12 +0200

    make the default scripts create filenames differentiated by host port

--- script/load.d/200-serverexec.lua
+++ script/load.d/200-serverexec.lua
@@ -9,18 +9,25 @@
 
 local Lr = require"utils.lambda".Lr
 
-os.execute('rm -f serverexec')
-local ok, unix, cmdpipe = pcall(Lr"require'socket.unix', require'std.cmdpipe'")
-if not ok then return engine.writelog("Cannot load luasocket with unix socket domain support, serverexec will not be available.") end
-local pipein = unix()
-if pipein:bind("serverexec") ~= 1 or pipein:listen() ~= 1 then
-  pipein:close()
-  engine.writelog("Cannot listen on serverexec")
-else
-  local unixpipe = cmdpipe.create(pipein, true, Lr"engine.writelog((_2 and 'new' or 'closed') .. ' connection to serverexec (' .. _1:getfd() .. ')')")
-  spaghetti.addhook("shuttingdown", function()
-    os.execute('rm -f serverexec')
-    unixpipe:close()
-  end)
-  unixpipe:selfservice()
-end
+--initialize after the port has been chosen
+local run
+run = spaghetti.addhook("tick", function()
+  spaghetti.removehook(run)
+  local fname = "serverexec-" .. cs.serverport
+  os.execute("rm -f " .. fname)
+  local ok, unix, cmdpipe = pcall(Lr"require'socket.unix', require'std.cmdpipe'")
+  if not ok then return engine.writelog("Cannot load luasocket with unix socket domain support, serverexec will not be available.") end
+  local pipein = unix()
+  if pipein:bind(fname) ~= 1 or pipein:listen() ~= 1 then
+    pipein:close()
+    engine.writelog("Cannot listen on " .. fname)
+  else
+    local unixpipe = cmdpipe.create(pipein, true, Lr"engine.writelog((_2 and 'new' or 'closed') .. ' connection to serverexec (' .. _1:getfd() .. ')')")
+    spaghetti.addhook("shuttingdown", function()
+      os.execute("rm -f " .. fname)
+      unixpipe:close()
+    end)
+    unixpipe:selfservice()
+  end
+end)
+
