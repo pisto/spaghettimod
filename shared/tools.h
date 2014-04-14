91cc995022455d608883f918c5481c52ee7604f7
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-04-14 16:57:28 +0000

    addunique/removeobj/replacewithlast cleanups
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4965 07af1644-1164-4067-b897-63bf4dc9b748

--- shared/tools.h
+++ shared/tools.h
@@ -593,9 +593,20 @@ template <class T> struct vector
         return -1;
     }
 
+    void addunique(const T &o)
+    {
+        if(find(o) < 0) add(o);
+    }
+
     void removeobj(const T &o)
     {
-        loopi(ulen) if(buf[i]==o) remove(i--);
+        loopi(ulen) if(buf[i] == o)
+        {
+            int dst = i;
+            for(int j = i+1; j < ulen; j++) if(buf[j] != o) buf[dst++] = buf[j];
+            setsize(dst);
+            break;
+        }
     }
 
     void replacewithlast(const T &o)
@@ -604,6 +615,7 @@ template <class T> struct vector
         loopi(ulen-1) if(buf[i]==o)
         {
             buf[i] = buf[ulen-1];
+            break;
         }
         ulen--;
     }
