ae218d390572ee2782256409051d62b82b689753
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-05-04 15:35:15 +0000

    getstring/filtertext cleanup
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4980 07af1644-1164-4067-b897-63bf4dc9b748

--- shared/tools.h
+++ shared/tools.h
@@ -1203,9 +1203,9 @@ extern float getfloat(ucharbuf &p);
 extern void sendstring(const char *t, ucharbuf &p);
 extern void sendstring(const char *t, packetbuf &p);
 extern void sendstring(const char *t, vector<uchar> &p);
-extern void getstring(char *t, ucharbuf &p, int len);
-template<size_t N> static inline void getstring(char (&t)[N], ucharbuf &p) { getstring(t, p, int(N)); }
-extern void filtertext(char *dst, const char *src, bool whitespace = true, int len = sizeof(string)-1);
+extern void getstring(char *t, ucharbuf &p, size_t len);
+template<size_t N> static inline void getstring(char (&t)[N], ucharbuf &p) { getstring(t, p, N); }
+extern void filtertext(char *dst, const char *src, bool whitespace = true, size_t len = sizeof(string)-1);
 
 #endif
 
