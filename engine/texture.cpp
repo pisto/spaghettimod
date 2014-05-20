2bc88cbfea1ac0ccb4d72afea60974a360857f8d
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-05-20 19:26:51 +0000

    IMG_LoadTyped_RW workaround
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@4994 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/texture.cpp
+++ engine/texture.cpp
@@ -1096,7 +1096,7 @@ SDL_Surface *loadsurface(const char *name)
         SDL_RWops *rw = z->rwops();
         if(rw) 
         {
-            const char *ext = strrchr(name, '.');
+            char *ext = (char *)strrchr(name, '.');
             if(ext) ++ext;
             s = IMG_LoadTyped_RW(rw, 0, ext);
             SDL_FreeRW(rw);
