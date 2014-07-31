5497533ec91dd76837d8df2c67340c1135716651
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-07-31 21:47:50 +0000

    empty wpcache fixes
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@5009 07af1644-1164-4067-b897-63bf4dc9b748

--- fpsgame/waypoint.cpp
+++ fpsgame/waypoint.cpp
@@ -79,8 +79,11 @@ namespace ai
                 bbmax.max(vec(w.o).add(radius));
             }
             if(first < last) lastwp = max(lastwp, last-1);
-            nodes.reserve(indices.length());
-            build(indices.getbuf(), indices.length(), bbmin, bbmax);
+            if(indices.length())
+            {
+                nodes.reserve(indices.length());
+                build(indices.getbuf(), indices.length(), bbmin, bbmax);
+            }
         }
 
         void build(int *indices, int numindices, const vec &vmin, const vec &vmax)
@@ -223,7 +226,7 @@ namespace ai
         } while(0)
         int closest = -1;
         wpcache::node *curnode;
-        loop(which, NUMWPCACHES) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
+        loop(which, NUMWPCACHES) if(wpcaches[which].firstwp >= 0) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
         {
             int axis = curnode->axis();
             float dist1 = pos[axis] - curnode->split[0], dist2 = curnode->split[1] - pos[axis];
@@ -277,7 +280,7 @@ namespace ai
             } \
         } while(0)
         wpcache::node *curnode;
-        loop(which, NUMWPCACHES) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
+        loop(which, NUMWPCACHES) if(wpcaches[which].firstwp >= 0) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
         {
             int axis = curnode->axis();
             float dist1 = pos[axis] - curnode->split[0], dist2 = curnode->split[1] - pos[axis];
@@ -329,7 +332,7 @@ namespace ai
             } \
         } while(0)
         wpcache::node *curnode;
-        loop(which, NUMWPCACHES) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
+        loop(which, NUMWPCACHES) if(wpcaches[which].firstwp >= 0) for(curnode = &wpcaches[which].nodes[0], wpcachestack.setsize(0);;)
         {
             int axis = curnode->axis();
             float dist1 = pos[axis] - curnode->split[0], dist2 = curnode->split[1] - pos[axis];
