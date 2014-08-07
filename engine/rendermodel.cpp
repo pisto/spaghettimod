e579564216b8961b4ee08b1ef8dde530e867e459
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-08-07 21:21:51 +0000

    remove some persistent vars
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@5012 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/rendermodel.cpp
+++ engine/rendermodel.cpp
@@ -1,7 +1,7 @@
 #include "engine.h"
 
-VARP(oqdynent, 0, 1, 1);
-VARP(animationinterpolationtime, 0, 150, 1000);
+VAR(oqdynent, 0, 1, 1);
+VAR(animationinterpolationtime, 0, 150, 1000);
 
 model *loadingmodel = NULL;
 
