9bcb79bcd4179e50b26d0b6ae4536a4d69a65cfc
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-09-04 22:28:09 +0000

    ping coloring
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@5016 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/serverbrowser.cpp
+++ engine/serverbrowser.cpp
@@ -215,15 +215,53 @@ int connectwithtimeout(ENetSocket sock, const char *hostname, const ENetAddress 
     return -1;
 }
  
+struct pingattempts
+{
+    enum { MAXATTEMPTS = 2 };
+
+    int offset, attempts[MAXATTEMPTS];
+
+    pingattempts() : offset(0) { clearattempts(); }
+
+    void clearattempts() { memset(attempts, 0, sizeof(attempts)); }
+
+    void setoffset() { offset = 1 + rnd(0xFFFFFF); } 
+
+    int encodeping(int millis)
+    {
+        return (millis ? millis : 1) + offset;
+    }
+
+    int decodeping(int val)
+    {
+        return val - offset;
+    }
+
+    int addattempt(int millis)
+    {
+        int val = encodeping(millis);
+        loopk(MAXATTEMPTS-1) attempts[k+1] = attempts[k];
+        attempts[0] = val;
+        return val;
+    }
+
+    bool checkattempt(int val)
+    {
+        loopk(MAXATTEMPTS) if(attempts[k] == val) return true;
+        return false;
+    }
+
+};
+
 enum { UNRESOLVED = 0, RESOLVING, RESOLVED };
 
-struct serverinfo
+struct serverinfo : pingattempts
 {
     enum 
     { 
         WAITING = INT_MAX,
 
-        MAXPINGS = 3 
+        MAXPINGS = 3
     };
 
     string name, map, sdesc;
@@ -235,10 +273,11 @@ struct serverinfo
     const char *password;
 
     serverinfo()
-     : port(-1), numplayers(0), resolved(UNRESOLVED), keep(false), password(NULL)
+        : port(-1), numplayers(0), resolved(UNRESOLVED), keep(false), password(NULL)
     {
         name[0] = map[0] = sdesc[0] = '\0';
         clearpings();
+        setoffset();
     }
 
     ~serverinfo()
@@ -253,6 +292,7 @@ struct serverinfo
         nextping = 0;
         lastping = -1;
         lastpong = 0;
+        clearattempts();
     }
 
     void cleanup()
@@ -368,6 +408,16 @@ VARP(servpingrate, 1000, 5000, 60000);
 VARP(servpingdecay, 1000, 15000, 60000);
 VARP(maxservpings, 0, 10, 1000);
 
+pingattempts lanattempts;
+
+template<size_t N> static inline void buildping(ENetBuffer &buf, uchar (&ping)[N], pingattempts &a)
+{
+    ucharbuf p(ping, N);
+    putint(p, a.addattempt(totalmillis));
+    buf.data = ping;
+    buf.dataLength = p.length();
+}
+
 void pingservers()
 {
     if(pingsock == ENET_SOCKET_NULL) 
@@ -380,11 +430,12 @@ void pingservers()
         }
         enet_socket_set_option(pingsock, ENET_SOCKOPT_NONBLOCK, 1);
         enet_socket_set_option(pingsock, ENET_SOCKOPT_BROADCAST, 1);
+
+        lanattempts.setoffset();
     }
+
     ENetBuffer buf;
     uchar ping[MAXTRANS];
-    ucharbuf p(ping, sizeof(ping));
-    putint(p, totalmillis ? totalmillis : 1);
 
     static int lastping = 0;
     if(lastping >= servers.length()) lastping = 0;
@@ -393,8 +444,7 @@ void pingservers()
         serverinfo &si = *servers[lastping];
         if(++lastping >= servers.length()) lastping = 0;
         if(si.address.host == ENET_HOST_ANY) continue;
-        buf.data = ping;
-        buf.dataLength = p.length();
+        buildping(buf, ping, si);
         enet_socket_send(pingsock, &si.address, &buf, 1);
         
         si.checkdecay(servpingdecay);
@@ -404,8 +454,7 @@ void pingservers()
         ENetAddress address;
         address.host = ENET_HOST_BROADCAST;
         address.port = server::laninfoport();
-        buf.data = ping;
-        buf.dataLength = p.length();
+        buildping(buf, ping, lanattempts);
         enet_socket_send(pingsock, &address, &buf, 1);
     }
     lastinfo = totalmillis;
@@ -460,12 +509,23 @@ void checkpings()
     {
         int len = enet_socket_receive(pingsock, &addr, &buf, 1);
         if(len <= 0) return;  
+        ucharbuf p(ping, len);
+        int millis = getint(p);
         serverinfo *si = NULL;
         loopv(servers) if(addr.host == servers[i]->address.host && addr.port == servers[i]->address.port) { si = servers[i]; break; }
-        if(!si && searchlan) si = newserver(NULL, server::serverport(addr.port), addr.host); 
-        if(!si || !si->limitpong()) continue;
-        ucharbuf p(ping, len);
-        int millis = getint(p), rtt = clamp(totalmillis - millis, 0, min(servpingdecay, totalmillis));
+        if(si)
+        {
+            if(!si->limitpong() || !si->checkattempt(millis)) continue;
+            millis = si->decodeping(millis);
+        }
+        else if(!searchlan || !lanattempts.checkattempt(millis)) continue;
+        else
+        {
+            si = newserver(NULL, server::serverport(addr.port), addr.host); 
+            si->limitpong();
+            millis = lanattempts.decodeping(millis);
+        }
+        int rtt = clamp(totalmillis - millis, 0, min(servpingdecay, totalmillis));
         if(millis >= lastreset && rtt < servpingdecay) si->addping(rtt, millis);
         si->numplayers = getint(p);
         int numattr = getint(p);
