d9407e925a1353ef4e827a14bbf1c2bb1fb5556d
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-09-19 19:21:13 +0200

    log connection floods

--- script/load.d/2300-connetcookies.lua
+++ script/load.d/2300-connetcookies.lua
@@ -8,6 +8,11 @@ local later = require"utils.later"
 
 later.later(1, function()
   if not engine.serverhost then return end
-  engine.enet_host_connect_cookies(engine.serverhost, 0)
+  if engine.enet_host_connect_cookies(engine.serverhost, 0) ~= 0 then return end
+  later.later(engine.ENET_HOST_DEFAULT_CONNECTING_PEER_TIMEOUT, function()
+    local connrate = engine.serverhost.connectsInWindow / engine.ENET_HOST_DEFAULT_CONNECTING_PEER_TIMEOUT * 1000
+    if connrate < 100 then return end
+    engine.writelog("Connection flood " .. connrate .. "/s \t(window size " .. engine.serverhost.connectsWindow .. " " .. math.modf(engine.serverhost.connectsInWindow / engine.serverhost.connectsWindow * 100) .. "% used)")
+  end, true)
 end)
 
