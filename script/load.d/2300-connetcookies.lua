b75c2b1073ae7fd7b90af44091bf4a416db87106
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-09-20 14:37:05 +0200

    cleanup

--- script/load.d/2300-connetcookies.lua
+++ script/load.d/2300-connetcookies.lua
@@ -15,7 +15,7 @@ later.later(1, function()
   if engine.enet_socket_set_option(engine.serverhost.socket, engine.ENET_SOCKOPT_SNDBUF, 5000000) == -1 then
     engine.writelog("Cannot set send buffer size")
   end
-  later.later(500, function()
+  later.later(1000, function()
     local connrate = engine.serverhost.connectsInWindow * 2
     if connrate < 100 then return end
     engine.writelog("Connection flood " .. connrate .. "/s \t(window size " .. engine.serverhost.connectsWindow .. " " .. math.modf(engine.serverhost.connectsInWindow / engine.serverhost.connectsWindow * 100) .. "% used)")
