4b3cc16db999eb55add063912fefc93dbc445b3d
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-09-20 12:51:43 +0200

    use 0.5 seconds timeout for cookies in default scripts (cuts off players with ping > 333ms, use one fourth of ram for cookies)

--- script/load.d/2300-connetcookies.lua
+++ script/load.d/2300-connetcookies.lua
@@ -8,15 +8,15 @@ local later = require"utils.later"
 
 later.later(1, function()
   if not engine.serverhost then return end
-  if engine.enet_host_connect_cookies(engine.serverhost, 0, 0) ~= 0 then return end
+  if engine.enet_host_connect_cookies(engine.serverhost, 1000, 0) ~= 0 then return end
   if engine.enet_socket_set_option(engine.serverhost.socket, engine.ENET_SOCKOPT_RCVBUF, 5000000) == -1 then
     engine.writelog("Cannot set receive buffer size")
   end
   if engine.enet_socket_set_option(engine.serverhost.socket, engine.ENET_SOCKOPT_SNDBUF, 5000000) == -1 then
     engine.writelog("Cannot set send buffer size")
   end
-  later.later(engine.ENET_HOST_DEFAULT_CONNECTING_PEER_TIMEOUT, function()
-    local connrate = engine.serverhost.connectsInWindow / engine.ENET_HOST_DEFAULT_CONNECTING_PEER_TIMEOUT * 1000
+  later.later(500, function()
+    local connrate = engine.serverhost.connectsInWindow * 2
     if connrate < 100 then return end
     engine.writelog("Connection flood " .. connrate .. "/s \t(window size " .. engine.serverhost.connectsWindow .. " " .. math.modf(engine.serverhost.connectsInWindow / engine.serverhost.connectsWindow * 100) .. "% used)")
   end, true)
