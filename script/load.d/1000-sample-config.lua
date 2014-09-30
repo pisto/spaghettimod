9c7a69dfdf9e4ddcf96b8bf30227fd9cb681f33e
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-10-01 00:12:12 +0200

    my pisto.horse domain is active!

--- script/load.d/1000-sample-config.lua
+++ script/load.d/1000-sample-config.lua
@@ -29,7 +29,7 @@ local protectdb = nameprotect.on(true)
 protectdb["^pisto$"] = { pisto = { pisto = true } }
 protectdb["[Dd]ino_?[Mm]artino"] = { pisto = { Dino_Martino = true } }
 
-cs.serverdesc = "\f7github:\f2spaghettimod"
+cs.serverdesc = "\f7pisto.horse 1024"
 
 cs.lockmaprotation = 2
 cs.maprotationreset()
