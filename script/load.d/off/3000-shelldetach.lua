926eba305c85e2d7f358597d9e1103b21903d335
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-10-27 12:55:55 +0100

    fork 0.1 seconds after initialization, use _Exit to prevent parent process to call destructors

--- script/load.d/off/3000-shelldetach.lua
+++ script/load.d/off/3000-shelldetach.lua
@@ -36,11 +36,14 @@ if os.getenv"RESTART" then
   return
 end
 
-engine.writelog("Detaching...")
-engine.setlogfile(require"std.servertag".fntag .. "log")
-io.stdin:close()
-io.stdout:close()
-io.stderr:close()
-if posix.fork() ~= 0 then os.exit(0) end
-engine.writelog("Detached.")
+spaghetti.later(100, function()
+  if spaghetti.quit then return end
+  engine.writelog("Detaching...")
+  engine.setlogfile(require"std.servertag".fntag .. "log")
+  io.stdin:close()
+  io.stdout:close()
+  io.stderr:close()
+  if posix.fork() ~= 0 then spaghetti._Exit(0) end
+  engine.writelog("Detached.")
+end)
 
