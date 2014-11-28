a6e685ffb22bf9a2ce0ec7a00f56969c6b9292f6
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-11-28 02:49:14 +0100

    support multiple flags for one holder in rugby mode

--- script/gamemods/rugby.lua
+++ script/gamemods/rugby.lua
@@ -5,20 +5,22 @@
 ]]--
 
 local module = {}
-local dodamagehook
+local map = require"utils.fp".map
 
+local dodamagehook
 function module.on(state)
   if dodamagehook then spaghetti.removehook(dodamagehook) dodamagehook = nil end
   if not state then return end
   dodamagehook = spaghetti.addhook("dodamage", function(info)
     if info.skip or not server.m_ctf or info.target.team ~= info.actor.team or info.gun ~= server.GUN_RIFLE then return end
-    local flags, actorflag = server.ctfmode.flags, {}
-    for i = 0, flags:length() - 1 do if flags[i].owner == info.actor.clientnum then actorflag[i] = true end end
-    actorflag = next(actorflag)
-    if not actorflag then return end
+    local flags, actorflags = server.ctfmode.flags, {}
+    for i = 0, flags:length() - 1 do if flags[i].owner == info.actor.clientnum then actorflags[i] = true end end
+    if not next(actorflags) then return end
     info.skip = true
-    server.ctfmode:returnflag(actorflag, 0)
-    server.ctfmode:takeflag(info.target, actorflag, flags[actorflag].version)
+    map.np(function(flag)
+      server.ctfmode:returnflag(flag, 0)
+      server.ctfmode:takeflag(info.target, flag, flags[flag].version)
+    end, actorflags)
   end)
 end
 
