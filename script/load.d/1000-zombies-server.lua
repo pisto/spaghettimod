0124ed5d7db93b4d80a90040c48c1cadd078ef6c
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-11-29 16:09:43 +0100

    zombies outbreak server on port 6666

---
+++ script/load.d/1000-zombies-server.lua
@@ -0,0 +1,144 @@
+--[[
+
+  A server which only runs zombie outbreak mode.
+
+]]--
+
+if not os.getenv("ZOMBIEVPS") then return end
+engine.writelog("Applying the zombie configuration.")
+
+require"std.servertag".tag = "zombie"
+
+local uuid = require"std.uuid"
+
+local fp, lambda = require"utils.fp", require"utils.lambda"
+local map, range, fold, last, I, L, Lr = fp.map, fp.range, fp.fold, fp.last, fp.I, lambda.L, lambda.Lr
+local abuse, playermsg, commands = require"std.abuse", require"std.playermsg", require"std.commands"
+
+cs.maxclients = 42
+cs.serverport = 6666
+
+--make sure you delete the next two lines, or I'll have admin on your server.
+cs.serverauth = "pisto"
+cs.adduser("pisto", "pisto", "+8ce1687301aea5c4500df0042849191f875c70555c3cc4c9", "a")
+require("std.preauth").on("pisto")
+
+local nameprotect = require"std.nameprotect"
+local protectdb = nameprotect.on(true)
+protectdb["^pisto$"] = { pisto = { pisto = true } }
+
+cs.serverdesc = "\f7 ZOMBIE OUTBREAK!"
+
+cs.lockmaprotation = 2
+cs.maprotationreset()
+--copied from data/menus.cfg
+local ffamaps, capturemaps, ctfmaps = table.concat({
+  "aard3c academy akaritori alithia alloy aqueducts arbana bvdm_01 castle_trap collusion complex corruption curvedm curvy_castle darkdeath deathtek depot",
+  "dirtndust DM_BS1 dock douze duel7 duel8 dune elegy fanatic_quake force fragplaza frostbyte frozen fury guacamole gubo hades",
+"hashi hog2 industry injustice island justice kalking1 katrez_d kffa killfactory kmap5 konkuri-to ksauer1 legazzo lostinspace masdm mbt10",
+  "mbt2 mbt9 memento metl2 metl3 metl4 moonlite neondevastation neonpanic nmp8 nucleus oasis oddworld ogrosupply orbe orion osiris",
+  "ot outpost paradigm park pgdm phosgene pitch_black powerplant refuge renegade rm5 roughinery ruby ruine sauerstruck sdm1 shadowed",
+  "shindou shinmei1 shiva simplicity skrdm1 stemple suburb tartech teahupoo tejen thetowers thor torment tumwalk turbine wake5 wdcd"
+}, " "), table.concat({
+  "abbey akroseum alithia arabic asgard asteroids c_egypt c_valley campo capture_night caribbean collusion core_refuge core_transfer corruption cwcastle damnation",
+  "dirtndust donya duomo dust2 eternal_valley evilness face-capture fb_capture fc3 fc4 fc5 forge frostbyte hades hallo haste hidden",
+  "infamy killcore3 kopenhagen lostinspace mbt12 mercury monastery nevil_c nitro nmp4 nmp8 nmp9 nucleus ogrosupply paradigm ph-capture reissen",
+  "relic river_c serenity snapper_rocks spcr subterra suburb tempest tortuga turbulence twinforts urban_c valhalla venice xenon"
+}, " "), table.concat({
+  "abbey akroseum arbana asgard authentic autumn bad_moon berlin_wall bt_falls campo capture_night catch22 core_refuge core_transfer damnation desecration dust2",
+  "eternal_valley europium evilness face-capture flagstone forge forgotten garden hallo haste hidden infamy kopenhagen l_ctf mach2 mbt1 mbt12",
+  "mbt4 mercury mill nitro nucleus recovery redemption reissen sacrifice shipwreck siberia snapper_rocks spcr subterra suburb tejen tempest",
+  "tortuga turbulence twinforts urban_c valhalla wdcd xenon"
+}, " ")
+
+ffamaps, capturemaps, ctfmaps = map.uv(function(maps)
+  local t = map.f(I, maps:gmatch("[^ ]+"))
+  map.nf(function(i)
+    local j = math.random(i)
+    local s = t[j]
+    t[j] = t[i]
+    t[i] = s
+  end, range.z(2, #t))
+  return table.concat(t, " ")
+end, ffamaps, capturemaps, ctfmaps)
+
+cs.maprotation("efficteam", ffamaps)
+cs.publicserver = 2
+
+require("std.flushinterval").set(5)
+
+--gamemods
+
+require"gamemods.zombieoutbreak".on(15, 10000/100*15)
+
+--moderation
+
+require"std.ban"
+cs.teamkillkick("*", 7, 30)
+
+--limit reconnects when banned, or to avoid spawn wait time
+abuse.reconnectspam(1/60, 5)
+
+--limit some message types
+spaghetti.addhook(server.N_KICK, function(info)
+  if info.skip or info.ci.privilege > server.PRIV_MASTER then return end
+  info.skip = true
+  playermsg("No. Use gauth.", info.ci)
+end)
+spaghetti.addhook(server.N_SOUND, function(info)
+  if info.skip or abuse.clientsound(info.sound) then return end
+  info.skip = true
+  playermsg("I know I used to do that but... whatever.", info.ci)
+end)
+abuse.ratelimit({ server.N_TEXT, server.N_SAYTEAM }, 0.5, 10, Lr"nil, 'I don\\'t like spam.'")
+abuse.ratelimit(server.N_SWITCHNAME, 1/30, 4, Lr"nil, 'You\\'re a pain.'")
+abuse.ratelimit(server.N_MAPVOTE, 1/10, 3, Lr"nil, 'That map sucks anyway.'")
+abuse.ratelimit(server.N_SPECTATOR, 1/30, 5, Lr"_.ci.clientnum ~= _.spectator, 'Can\\'t even describe you.'") --self spec
+abuse.ratelimit(server.N_MASTERMODE, 1/30, 5, Lr"_.ci.privilege == server.PRIV_NONE, 'Can\\'t even describe you.'")
+abuse.ratelimit({ server.N_AUTHTRY, server.N_AUTHKICK }, 1/60, 4, Lr"nil, 'Are you really trying to bruteforce a 192 bits number? Kudos to you!'")
+abuse.ratelimit(server.N_CLIENTPING, 4.5) --no message as it could be cause of network jitter
+abuse.ratelimit(server.N_SERVCMD, 0.5, 10, Lr"nil, 'Yes I\\'m filtering this too.'")
+
+--prevent masters from annoying players
+local tb = require"utils.tokenbucket"
+local function bullying(who, victim)
+  local t = who.extra.bullying or {}
+  local rate = t[victim.extra.uuid] or tb(1/30, 6)
+  t[victim.extra.uuid] = rate
+  who.extra.bullying = t
+  return not rate()
+end
+spaghetti.addhook(server.N_SETTEAM, function(info)
+  if info.skip or info.who == info.sender or not info.wi or info.ci.privilege == server.PRIV_NONE then return end
+  local team = engine.filtertext(info.text):sub(1, engine.MAXTEAMLEN)
+  if #team == 0 or team == info.wi.team then return end
+  if bullying(info.ci, info.wi) then
+    info.skip = true
+    playermsg("...", info.ci)
+  end
+end)
+spaghetti.addhook(server.N_SPECTATOR, function(info)
+  if info.skip or info.spectator == info.sender or not info.spinfo or info.ci.privilege == server.PRIV_NONE or info.val == (info.spinfo.state.state == engine.CS_SPECTATOR and 1 or 0) then return end
+  if bullying(info.ci, info.spinfo) then
+    info.skip = true
+    playermsg("...", info.ci)
+  end
+end)
+
+--ratelimit just gobbles the packet. Use the selector to add a tag to the exceeding message, and append another hook to send the message
+local function warnspam(packet)
+  if not packet.ratelimited or type(packet.ratelimited) ~= "string" then return end
+  playermsg(packet.ratelimited, packet.ci)
+end
+map.nv(function(type) spaghetti.addhook(type, warnspam) end,
+  server.N_TEXT, server.N_SAYTEAM, server.N_SWITCHNAME, server.N_MAPVOTE, server.N_SPECTATOR, server.N_MASTERMODE, server.N_AUTHTRY, server.N_AUTHKICK, server.N_CLIENTPING
+)
+
+--simple banner
+
+local git = io.popen("echo `git rev-parse --short HEAD` `git show -s --format=%ci`")
+local gitversion = git:read()
+git = nil, git:close()
+commands.add("info", function(info)
+  playermsg("spaghettimod is a reboot of hopmod for programmers. Will be used for SDoS.\nKindly brought to you by pisto." .. (gitversion and "\nCommit " .. gitversion or ""), info.ci)
+end)
