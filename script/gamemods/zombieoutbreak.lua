82740fb3ed560962fbdf40e0e957d6fa1cfca9ce
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-01 22:44:42 +0100

    fix empty server detectoin in zombies

--- script/gamemods/zombieoutbreak.lua
+++ script/gamemods/zombieoutbreak.lua
@@ -181,8 +181,8 @@ function module.on(speed, spawninterval, persist)
   hooks.disconnect = spaghetti.addhook("clientdisconnect", function(info)
     if not active then return end
     local tot = 0
-    map.nf(function() tot = tot + 1 end, iterators.clients())
-    if tot == 1 then server.checkvotes(true) return end
+    map.nf(function(ci) tot = tot + (info.ci.clientnum == ci.clientnum and 0 or 1) end, iterators.clients())
+    if tot == 0 then server.checkvotes(true) return end
     if gracetime then return end
     changeteam(info.ci, "evil")
     guydown(info.ci, true, persist)
