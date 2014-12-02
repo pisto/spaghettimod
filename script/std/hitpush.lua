169207592bffb788e20a7ddf53ff060d63b2cb6e
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-03 00:42:41 +0100

    send ownernum in N_HISTPUSH

--- script/std/hitpush.lua
+++ script/std/hitpush.lua
@@ -10,6 +10,6 @@ return function(ci, vel, p)
   local ret = p
   p = putf(p or { 20, engine.ENET_PACKET_FLAG_RELIABLE }, server.N_HITPUSH, ci.clientnum, 0, 1, vel.x * 100 / 80 * server.DNF, vel.y * 100 / 80 * server.DNF, vel.z * 100 / 80 * server.DNF)
   if ret then return p end
-  engine.sendpacket(ci.clientnum, 1, p:finalize(), -1)
+  engine.sendpacket(ci.ownernum, 1, p:finalize(), -1)
   ci:setpushed()
 end
