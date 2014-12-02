d4914886230718ff4d80e8db6cd6e7bdee5e6323
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-02 20:52:51 +0100

    automatic utf8 transcoding in the jsonpersist module

--- script/utils/jsonpersist.lua
+++ script/utils/jsonpersist.lua
@@ -4,13 +4,25 @@
 
 ]]--
 
-local json = require"json"
-
+local json, fp = require"json", require"utils.fp"
+local map = fp.map
 
 local module = {}
 
-function module.save(t, fname)
-  local j = json.encode(t)
+local function utf8translate(transcode)
+  local self
+  self = function(obj)
+    local t = type(obj)
+    if t == "string" then return transcode(obj)
+    elseif t == "table" then return map.mp(function(k, v) return self(k), self(v) end, obj)
+    else return obj end
+  end
+  return self
+end
+local toutf8, fromutf8 = utf8translate(engine.encodeutf8), utf8translate(engine.decodeutf8)
+
+function module.save(t, fname, notranscode, options)
+  local j = json.encode(notranscode and t or toutf8(t), options)
   local out, err, ok = io.open("var/" .. fname .. ".next", "w")
   if not out then error(err) end
   ok, err = pcall(out.write, out, j)
@@ -20,13 +32,14 @@ function module.save(t, fname)
   if not ok then error(err) end
 end
 
-function module.load(fname, fatal)
+function module.load(fname, fatal, notranscode, options)
   local iin, err = io.open("var/" .. fname)
   if not iin then if fatal then error(err) end return end
   local ok, str = pcall(iin.read, iin, "*a")
   iin:close()
   if not ok then error(str) end
-  return json.decode(str)
+  local t = json.decode(str, options)
+  return notranscode and t or fromutf8(t)
 end
 
 return module
