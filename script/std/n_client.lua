6eda955b7cc371c488c099840ee25db6f5e047f3
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-04 23:56:38 +0100

    n_client cleanup

--- script/std/n_client.lua
+++ script/std/n_client.lua
@@ -7,7 +7,9 @@
 local putf = require"std.putf"
 
 local function wrap(p, client)
-  return putf({ p.growth, p.packet.flags }, server.N_CLIENT, type(client) == "number" and client or client.clientnum, { uint = p.len }, { buf = p.buf:sub(1, p.len) })
+  local buf = p.buf
+  p.len = 0
+  return putf(p, server.N_CLIENT, type(client) == "number" and client or client.clientnum, { uint = p.len }, { buf = buf })
 end
 
 return wrap
