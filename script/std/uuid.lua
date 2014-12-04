266208d329c23c4281879c24f5600dc7b93998de
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-04 03:04:13 +0100

    cleanup std.uuid, remove dependency from std.iterators

--- script/std/uuid.lua
+++ script/std/uuid.lua
@@ -11,28 +11,12 @@ local urandom = io.open("/dev/urandom")
 uuid.randomseed(last(fold.vz(Lr"_1 * 256 + _2", 0, urandom:read(4):byte())) % 0x7FFFFFFF)
 urandom:close()
 
-local uuids = {}
-
-local function adduuid(info)
-  info.ci.extra.uuid = uuid()
-  uuids[info.ci.extra.uuid] = info.ci
-end
-
+local function adduuid(info) info.ci.extra.uuid = uuid() end
 spaghetti.addhook("clientconnect", adduuid)
 spaghetti.addhook("botjoin", adduuid)
 
-for ci in require("std.iterators").all() do
-  if not ci.extra.uuid then ci.extra.uuid = uuid() end
-end
-
-local iterators = require"std.iterators"
-local function find(uuid)
-  local function finder(...)
-    return first(pick.zf(function(ci)
-      return ci.extra.uuid == uuid
-    end, ...))
-  end
-  return finder(iterators.all()) or finder(iterators.connects())
-end
-
-return { find = find }
+return { find = function(ciuuid)
+  local l1, l2 = server.clients, server.connects
+  for i = 0, l1:length() - 1 do if l1[i].extra.uuid == ciuuid then return l1[i] end end
+  for i = 0, l2:length() - 1 do if l2[i].extra.uuid == ciuuid then return l2[i] end end
+end }
