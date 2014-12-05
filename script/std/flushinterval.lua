1e0aad4626ef0440f5d85d0fd9ffcc961bd7898f
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-05 14:17:16 +0100

    do not hog cpu with std.flushinterval if there is no client

--- script/std/flushinterval.lua
+++ script/std/flushinterval.lua
@@ -4,20 +4,29 @@
 
 ]]--
 
-local fp = require"utils.fp"
-
-local repeater
+local repeater, connected
 
 local function flusher()
+  if server.clients:length() == 0 then
+    spaghetti.cancel(repeater)
+    repeater = nil
+    return
+  end
   engine.flushserver(true)
   server.lastsend = engine.enet_time_get()
 end
 
 local function set(interval)
-  if repeater then repeater = nil, spaghetti.cancel(repeater) end
-  if interval == 33 then return end
-  flusher()
+  if repeater then
+    spaghetti.cancel(repeater)
+    spaghetti.removehook(connected)
+    repeater, connected = nil
+  end
+  if interval >= 33 then return end
   repeater = spaghetti.later(interval, flusher, true)
+  connected = spaghetti.addhook("connected", function()
+    repeater = repeater or spaghetti.later(interval, flusher, true)
+  end)
 end
 
 return {set = set}
