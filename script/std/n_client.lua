85c236147a0d9c1f408271de0c877e8aabce9c3f
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-05 01:13:18 +0100

    typo

--- script/std/n_client.lua
+++ script/std/n_client.lua
@@ -9,7 +9,7 @@ local putf = require"std.putf"
 local function wrap(p, client)
   local buf = p.buf
   p.len = 0
-  return putf(p, server.N_CLIENT, type(client) == "number" and client or client.clientnum, { uint = p.len }, { buf = buf })
+  return putf(p, server.N_CLIENT, type(client) == "number" and client or client.clientnum, { uint = #buf }, { buf = buf })
 end
 
 return wrap
