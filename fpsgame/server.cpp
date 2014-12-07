3c3b38dc0754bd54964ac4baa69e00c11ace65bc
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2014-12-07 21:16:48 +0000

    hashpassword cleanup
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@5050 07af1644-1164-4067-b897-63bf4dc9b748

--- fpsgame/server.cpp
+++ fpsgame/server.cpp
@@ -1338,7 +1338,7 @@ namespace server
     {
         char buf[2*sizeof(string)];
         formatstring(buf)("%d %d ", cn, sessionid);
-        copystring(&buf[strlen(buf)], pwd);
+        concatstring(buf, pwd, sizeof(buf));
         if(!hashstring(buf, result, maxlen)) *result = '\0';
     }
 
