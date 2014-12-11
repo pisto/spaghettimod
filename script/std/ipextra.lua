f10cf2308392d44088bd081135a744253e1aa2e0
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-12 00:38:40 +0100

    std.ipextra module: extra table attached to an ip

---
+++ script/std/ipextra.lua
@@ -0,0 +1,45 @@
+--[[
+
+  Persistent extra table attached to an IP, analogous to the extra table of a clientinfo.
+
+]]--
+
+local fp, iterators = require"utils.fp", require"std.iterators"
+local map = fp.map
+local module, extras, ttl = {}, {}, 10 * 60 * 1000
+
+local function ciip(ci)
+  return engine.ENET_NET_TO_HOST_32(engine.getclientpeer(ci.clientnum).address.host)
+end
+
+local pruner
+pruner = function(ip)
+  return function()
+    for ci in iterators.clients() do
+      if ciip(ci) == ip then spaghetti.later(ttl, pruner(ip)) return end
+    end
+    extras[ip] = nil
+  end
+end
+
+spaghetti.addhook("enetevent", function(info)
+  if info.event.type ~= engine.ENET_EVENT_TYPE_CONNECT then return end
+  local ip = engine.ENET_NET_TO_HOST_32(info.event.peer.address.host)
+  if extras[ip] then return end
+  extras[ip] = {}
+  spaghetti.later(ttl, pruner(ip))
+end)
+
+spaghetti.addhook("clientconnect", function(info)
+  info.ci.extra.ipextra = ciip(info.ci)
+end)
+
+function module.ttl(reqttl, force)
+  ttl = (reqttl > ttl or force) and reqttl or ttl
+end
+
+function module.find(ip)
+  return extras[ip]
+end
+
+return module
