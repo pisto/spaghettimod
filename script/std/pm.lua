b0b3e69a12648d0e9a4b56b0e41bb15a39d1dc7b
Lorenzo Pistone <blaffablaffa@gmail.com>
2014-12-20 04:00:04 +0100

    #pm message cleanup

--- script/std/pm.lua
+++ script/std/pm.lua
@@ -14,7 +14,7 @@ commands.add("pm", function(info)
   if cn == sendercn then return end
   local who = engine.getclientinfo(cn)
   if not who then playermsg("Cannot find cn " .. cn, info.ci) return end
-  local hdr = "[PM, reply #pm" .. sendercn .. "]:"
+  local hdr = "[PM, reply #pm " .. sendercn .. "]:"
   engine.sendpacket(who.clientnum, 1, putf({ 6 + #hdr + #msg, engine.ENET_PACKET_FLAG_RELIABLE }, server.N_SAYTEAM, sendercn, hdr, server.N_SAYTEAM, sendercn, msg):finalize(), -1)
   engine.writelog(("pm: %s (%d) => %s (%d): "):format(info.ci.name, sendercn, who.name, who.clientnum) .. msg)
 end, "#pm <cn> <text>: send text to client as a private message")
