82b4484a078340a93eafd908b895cdde4ea506a7
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-01-18 17:14:37 +0100

    std.sound, enable send to single client

--- script/std/sound.lua
+++ script/std/sound.lua
@@ -6,6 +6,6 @@
 
 local n_client, putf = require"std.n_client", require"std.putf"
 
-return function(ci, sound, unreliable)
-  engine.sendpacket(-1, 1, n_client(putf({4, unreliable and 0 or engine.ENET_PACKET_FLAG_RELIABLE}, server.N_SOUND, sound), ci):finalize(), -1)
+return function(ci, sound, private, unreliable)
+  engine.sendpacket(private and ci.ownernum or -1, 1, n_client(putf({4, unreliable and 0 or engine.ENET_PACKET_FLAG_RELIABLE}, server.N_SOUND, sound), ci):finalize(), -1)
 end
