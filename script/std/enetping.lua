b11d33ef98875a09accecabb1e6813e7c8896a10
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-02-08 02:54:29 +0100

    do not force the ping on its own client

--- script/std/enetping.lua
+++ script/std/enetping.lua
@@ -4,15 +4,12 @@
 
 ]]--
 
-local fp, L, putf, iterators, n_client = require"utils.fp", require"utils.lambda", require"std.putf", require"std.iterators", require"std.n_client"
+local fp, L, iterators = require"utils.fp", require"utils.lambda", require"std.iterators"
 
 spaghetti.addhook(server.N_CLIENTPING, L"_.skip = true")
-spaghetti.addhook(server.N_PING, L"_.skip = true")
 
 spaghetti.later(250, function() for ci in iterators.all() do
   local ping = engine.getclientpeer(ci.ownernum).roundTripTime
   ci.ping = ping
-  if ci.state.aitype == server.AI_NONE then
-    engine.sendpacket(-1, 1, n_client(putf({}, server.N_CLIENTPING, ping), ci):finalize(), -1)
-  end
+  if ci.state.aitype == server.AI_NONE then ci.messages:putint(server.N_CLIENTPING):putint(ping) end
 end end, true)
