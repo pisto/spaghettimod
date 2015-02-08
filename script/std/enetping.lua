158d0e208c1e14c92dce71e368a1d7ba366d4539
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-02-08 12:06:55 +0100

    force a round of N_CLIENTPING refresh when a client joins

--- script/std/enetping.lua
+++ script/std/enetping.lua
@@ -8,8 +8,14 @@ local fp, L, iterators = require"utils.fp", require"utils.lambda", require"std.i
 
 spaghetti.addhook(server.N_CLIENTPING, L"_.skip = true")
 
-spaghetti.later(250, function() for ci in iterators.all() do
-  local oldping
-  oldping, ci.ping = ci.ping, engine.getclientpeer(ci.ownernum).roundTripTime
-  if oldping ~= ci.ping and ci.state.aitype == server.AI_NONE then ci.messages:putint(server.N_CLIENTPING):putint(ci.ping) end
-end end, true)
+local refresh
+spaghetti.later(250, function()
+  for ci in iterators.all() do
+    local oldping
+    oldping, ci.ping = ci.ping, engine.getclientpeer(ci.ownernum).roundTripTime
+    if (refresh or oldping ~= ci.ping) and ci.state.aitype == server.AI_NONE then ci.messages:putint(server.N_CLIENTPING):putint(ci.ping) end
+  end
+  refresh = false
+end, true)
+
+spaghetti.addhook("connected", function() refresh = true end)
