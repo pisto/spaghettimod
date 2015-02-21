ae03bfc5ee053e0b05116854fa8de8cb54534691
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-02-21 15:11:14 +0100

    reimplement std.trackent, keep the trackent across deaths

--- script/std/trackent.lua
+++ script/std/trackent.lua
@@ -4,52 +4,69 @@
 
 ]]--
 
-local fp, L, ents, n_client, putf, iterators = require"utils.fp", require"utils.lambda", require"std.ents", require"std.n_client", require"std.putf", require"std.iterators"
+local fp, L, ents, n_client, putf = require"utils.fp", require"utils.lambda", require"std.ents", require"std.n_client", require"std.putf"
 local map, first = fp.map, fp.first
 
 require"std.lastpos"
 require"std.notalive"
 local module = {}
 
-function module.remove(ci, i)
+function module.remove(ci, i, nodelent)
   local trackents = ci.extra.trackents
   if not trackents or not trackents.ents[i] then return end
   trackents.ents[i] = nil
-  ents.delent(i)
+  if not nodelent then ents.delent(i) end
   if not next(trackents.ents) then
     map.np(L"spaghetti.removehook(_2)", trackents.hooks)
     ci.extra.trackents = nil
   end
 end
 
-local function trackclean(ci)
+local function trackclean(ci, nodelent)
   if not ci.extra.trackents then return end
-  for i in pairs(ci.extra.trackents.ents) do module.remove(ci, i) end
+  for i in pairs(ci.extra.trackents.ents) do module.remove(ci, i, nodelent) end
 end
 local function trackclean_hook(info) trackclean(info.ci) end
 
-function module.add(ci, entityupdater, reliable, hideself)
+local function makeeditent(p, i)
+  local _, _, ment = ents.getent(i)
+  return putf(p, server.N_EDITENT, i, ment.o.x * server.DMF, ment.o.y * server.DMF, ment.o.z * server.DMF, ment.type, ment.attr1, ment.attr2, ment.attr3, ment.attr4, ment.attr5)
+end
+
+function module.add(ci, updater, reliable, hideself)
   local trackent, owner = {}, ci.clientnum
-  local i = ents.newent(server.NOTUSED, nil, 0, 0, 0, 0, 0, function(i, who)
-    local _, sent, ment = ents.getent(i)
-    local p = n_client(putf({ 20, reliable and engine.ENET_PACKET_FLAG_RELIABLE or 0 }, server.N_EDITENT, i, ment.o.x * server.DMF, ment.o.y * server.DMF, ment.o.z * server.DMF, ment.type, ment.attr1, ment.attr2, ment.attr3, ment.attr4, ment.attr5), first(iterators.all()))
-    engine.sendpacket(who and who.clientnum or -1, 1, p:finalize(), hideself and ci.ownernum or -1)
-  end)
+  local i = ents.newent(server.NOTUSED, nil, 0, 0, 0, 0, 0, L"")
   if not i then return end
 
   if not ci.extra.trackents then
     ci.extra.trackents = { hooks = {
       position = spaghetti.addhook("positionupdate", function(info)
         if info.cp.clientnum ~= owner then return end
-        for i, entityupdater in pairs(ci.extra.trackents.ents) do entityupdater(i, info.lastpos) end
-      end),
-      notalive = spaghetti.addhook("notalive", trackclean_hook),
-      clientdisconnect = spaghetti.addhook("clientdisconnect", trackclean_hook),
-      botleave = spaghetti.addhook("botleave", trackclean_hook),
-      changemap = spaghetti.addhook("changemap", function() trackclean(ci) end)
+        for i, track in pairs(ci.extra.trackents.ents) do track.updater(i, info.lastpos) end
+      end, true),
+      worldstate_pos = spaghetti.addhook("worldstate_pos", function(info)
+        if info.ci.clientnum ~= ci.clientnum or ci.position:empty() then return end
+        local p = { [true] = {}, [false] = {} }
+        for i, t in pairs(ci.extra.trackents.ents) do
+          if t.reliable and t.hideself then makeeditent(ci.messages, i)
+          else p[t.hideself][t.reliable] = makeeditent(p[t.hideself][t.reliable] or { r = t.reliable }, i) end
+        end
+        for hideself, p in pairs(p) do for _, p in pairs(p) do
+          engine.sendpacket(-1, 1, n_client(p, ci):finalize(), hideself and ci.clientnum or -1)
+        end end
+      end, true),
+      notalive = spaghetti.addhook("notalive", function(info)
+        if info.ci.clientnum ~= ci.clientnum then return end
+        for i in pairs(ci.extra.trackents.ents) do
+          engine.sendpacket(-1, 1, n_client(putf({ 20, r = 1}, server.N_EDITENT, i, -1e7,  -1e7,  -1e7, server.NOTUSED, 0, 0, 0, 0, 0), ci):finalize(), -1)
+        end
+      end, true),
+      clientdisconnect = spaghetti.addhook("clientdisconnect", trackclean_hook, true),
+      botleave = spaghetti.addhook("botleave", trackclean_hook, true),
+      changemap = spaghetti.addhook("changemap", function() trackclean(ci, true) end, true)
     }, ents = {} }
   end
-  ci.extra.trackents.ents[i] = entityupdater
+  ci.extra.trackents.ents[i] = { updater = updater, reliable = not not reliable, hideself = not not hideself }
   return i
 
 end
