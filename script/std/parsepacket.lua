796fceda7696a78c77bde3d424a62e6daa4ef205
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-03-07 00:57:59 +0100

    N_EDITVAR std.parsepacket

--- script/std/parsepacket.lua
+++ script/std/parsepacket.lua
@@ -11,6 +11,12 @@ local parser = {
   [server.N_TEXT] = function(info) info.text = getf(info.p, "s") end,
   [server.N_AUTHTRY] = function(info) info.desc, info.name = getf(info.p, "ss") end,
   [server.N_AUTHANS] = function(info) info.desc, info.id, info.ans = getf(info.p, "sis") end,
+  [server.N_EDITVAR] = function(info)
+    info.stringval, info.numval, info.type_editvar, info.text = "", 0, getf(info.p, "is")
+    if info.type_editvar == engine.ID_SVAR then info.stringval = getf(info.p, "s")
+    elseif info.type_editvar == engine.ID_VAR then info.numval = getf(info.p, "i")
+    elseif info.type_editvar == engine.ID_FVAR then info.numval = getf(info.p, "f") end
+  end,
 }
 
 return function(info)
