ffeb6e62a4fa370126dfaaca34231ef8b898a1d2
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-03-12 16:00:54 +0100

    make the location of the definition of the lambda appear in stack traces

--- script/utils/lambda.lua
+++ script/utils/lambda.lua
@@ -1,10 +1,12 @@
 local compiled = {}
 
 return function(code)
-  local cached = compiled[code]
+  local defined = debug.getinfo(2, "nlS")
+  defined = ("<lambda %s:%d (%s)>"):format(defined.short_src, defined.currentline, defined.name)
+  local cached = compiled[code .. defined]
   if cached then return cached end
-  local L, err = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... return " .. code, "<lambda>")
-  if not L then L, err = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... " .. code, "<lambda>") end
-  compiled[code] = assert(L, "Cannot compile lambda: " .. tostring(err))
+  local L, err = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... return " .. code, defined)
+  if not L then L, err = load("local _, _1, _2, _3, _4, _5, _6, _7, _8, _9 = ..., ... " .. code, defined) end
+  compiled[code .. defined] = assert(L, "Cannot compile lambda: " .. tostring(err))
   return L
 end
