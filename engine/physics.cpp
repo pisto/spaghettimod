e289a3e8d161a4923f6281cc4c926a3e0e9e8f2f
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2015-06-22 00:47:16 +0000

    allowmove cleanup
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@5099 07af1644-1164-4067-b897-63bf4dc9b748

--- engine/physics.cpp
+++ engine/physics.cpp
@@ -1215,7 +1215,7 @@ bool trystepdown(physent *d, vec &dir, float step, float xy, float z, bool init 
 
 bool trystepdown(physent *d, vec &dir, bool init = false)
 {
-    if(!game::allowmove(d) || (!d->move && !d->strafe)) return false;
+    if((!d->move && !d->strafe) || !game::allowmove(d)) return false;
     vec old(d->o);
     d->o.z -= STAIRHEIGHT;
     d->zmargin = -STAIRHEIGHT;
@@ -1585,7 +1585,7 @@ void modifyvelocity(physent *pl, bool local, bool water, bool floating, int curt
     if(!floating && pl->physstate == PHYS_FALL) pl->timeinair += curtime;
 
     vec m(0.0f, 0.0f, 0.0f);
-    if(allowmove && (pl->move || pl->strafe))
+    if((pl->move || pl->strafe) && allowmove)
     {
         vecfromyawpitch(pl->yaw, floating || water || pl->type==ENT_CAMERA ? pl->pitch : 0, pl->move, pl->strafe, m);
 
