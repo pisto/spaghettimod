eb4ad62d792a74fc9e5be4c2a1d2169de4003371
eihrul <eihrul@07af1644-1164-4067-b897-63bf4dc9b748>
2015-07-12 18:10:18 +0000

    filtertext cleanup
    git-svn-id: https://svn.code.sf.net/p/sauerbraten/code/src@5149 07af1644-1164-4067-b897-63bf4dc9b748

--- shared/tools.h
+++ shared/tools.h
@@ -1274,7 +1274,8 @@ extern void sendstring(const char *t, packetbuf &p);
 extern void sendstring(const char *t, vector<uchar> &p);
 extern void getstring(char *t, ucharbuf &p, size_t len);
 template<size_t N> static inline void getstring(char (&t)[N], ucharbuf &p) { getstring(t, p, N); }
-extern void filtertext(char *dst, const char *src, bool whitespace = true, bool forcespace = false, size_t len = sizeof(string)-1);
+extern void filtertext(char *dst, const char *src, bool whitespace, bool forcespace, size_t len);
+template<size_t N> static inline void filtertext(char (&dst)[N], const char *src, bool whitespace = true, bool forcespace = false) { filtertext(dst, src, whitespace, forcespace, N-1); }
 
 struct ipmask
 {
