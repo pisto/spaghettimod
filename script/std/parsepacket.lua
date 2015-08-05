8bf6f16a3dd1cad7da2a79a348260c74c2ea9c01
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-08-05 23:23:24 +0200

    add std.parsepacket for servcmd

--- script/std/parsepacket.lua
+++ script/std/parsepacket.lua
@@ -9,6 +9,7 @@ local getf = require"std.getf"
 local parser = {
   [server.N_CONNECT] = function(info) info.text, info.playermodel, info.password, info.authdesc, info.authname = getf(info.p, "sisss") end,
   [server.N_TEXT] = function(info) info.text = getf(info.p, "s") end,
+  [server.N_SERVCMD] = function(info) info.text = getf(info.p, "s") end,
   [server.N_AUTHTRY] = function(info) info.desc, info.name = getf(info.p, "ss") end,
   [server.N_AUTHANS] = function(info) info.desc, info.id, info.ans = getf(info.p, "sis") end,
   [server.N_EDITVAR] = function(info)
