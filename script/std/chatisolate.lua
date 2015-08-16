6ce3c097451d6ec922af3b649a3ace2858a3bdfd
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-08-16 23:21:41 +0200

    log chat even when chatisolate is on

--- script/std/chatisolate.lua
+++ script/std/chatisolate.lua
@@ -25,21 +25,23 @@ local function on(on)
   hooks.text = spaghetti.addhook(server.N_TEXT, function(info)
     if info.skip or not hooks or info.ci.privilege > server.PRIV_NONE or info.ci.state.state ~= engine.CS_SPECTATOR then return end
     info.skip = true
-    local sendercn = info.ci.clientnum
-    local textp = n_client(putf({30, r=1}, server.N_TEXT, engine.filtertext(info.text, true, true)), info.ci):finalize()
+    local sendercn, text = info.ci.clientnum, engine.filtertext(info.text, true, true)
+    local textp = n_client(putf({30, r=1}, server.N_TEXT, text), info.ci):finalize()
     for ci in iterators.clients() do if sendercn ~= ci.clientnum and module.cantalkto(info.ci, ci) then
       engine.sendpacket(ci.clientnum, 1, textp, -1)
     end end
     server.recordpacket(1, textp.data)
+    engine.writelog(("%s: %s"):format(server.colorname(info.ci, nil), text))
   end)
   hooks.text = spaghetti.addhook(server.N_SAYTEAM, function(info)
-    if info.skip or not hooks or info.ci.privilege > server.PRIV_NONE or info.ci.state.state ~= engine.CS_SPECTATOR or not server.m_teammode then return end
+    if info.skip or not hooks or info.ci.privilege > server.PRIV_NONE or info.ci.state.state ~= engine.CS_SPECTATOR or not server.m_teammode or info.ci.team == "" then return end
     info.skip = true
-    local sendercn = info.ci.clientnum
-    local textp = putf({30, r=1}, server.N_SAYTEAM, sendercn, engine.filtertext(info.text, true, true)):finalize()
+    local sendercn, text = info.ci.clientnum, engine.filtertext(info.text, true, true)
+    local textp = putf({30, r=1}, server.N_SAYTEAM, sendercn, text):finalize()
     for ci in iterators.clients() do if sendercn ~= ci.clientnum and module.cantalkto(info.ci, ci) and ci.team == info.ci.team then
       engine.sendpacket(ci.clientnum, 1, textp, -1)
     end end
+    engine.writelog(("%s <%s>: %s"):format(server.colorname(info.ci, nil), info.ci.team, text))
   end)
 end
 
