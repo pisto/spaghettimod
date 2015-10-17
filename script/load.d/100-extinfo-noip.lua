783fd4de71973bad31645ebdcb4751f948c00235
Lorenzo Pistone <blaffablaffa@gmail.com>
2015-10-17 21:13:32 +0200

    simplify fake ip creation

--- script/load.d/100-extinfo-noip.lua
+++ script/load.d/100-extinfo-noip.lua
@@ -16,25 +16,11 @@ end
 engine.writelog("Generating fake extinfo IPs from geoip csv data...")
 local locations = {}
 for line in fakegeoip:lines() do
-  local s, e, code = line:match('"(%d+)","(%d+)","(..)"')
-  if s then
-    s, e = tonumber(s), tonumber(e)
-    local list = locations[code] or { tot = 0 }
-    table.insert(list, { s = s, tot = e - s + 1 })
-    list.tot = list.tot + e - s + 1
-    locations[code] = list
-  end
+  local ip, code = line:match('"(%d+)","%d+","([A-Z][A-Z])"')
+  if ip then locations[code] = ip end
 end
 fakegeoip:close()
-locations = map.mp(function(code, list)
-  local r = math.random(0, list.tot - 1)
-  for _, range in ipairs(list) do
-    if r >= range.tot then r = r - range.tot
-    else return code, range.s + r end
-  end
-end, locations)
 engine.writelog("... done")
-
 
 require"std.extinfo"
 spaghetti.addhook("extinfo", function(info)
