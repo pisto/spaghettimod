30b10286093933ce9b9eef45c4cec04230d42523
Lorenzo Pistone <blaffablaffa@gmail.com>
2016-01-15 13:05:45 +0100

    don't try to remove the server from the master list on shutdown

--- script/load.d/20-cleanshutdown.lua
+++ script/load.d/20-cleanshutdown.lua
@@ -1,6 +1,6 @@
 --[[
 
-  Send a goodbye message to clients before shutdown, and use a hacky way to remove us from the master server quickly.
+  Send a goodbye message to clients before shutdown.
 
 ]]--
 
@@ -8,8 +8,6 @@ spaghetti.addhook("shuttingdown", function(info)
   engine.writelog(info.servererror and "Shutting down (fatal)." or "Shutting down.")
   if info.servererror then return end
   server.sendservmsg("\f6This server is shutting down or rebooting.\nTry \f0/reconnect\f6 in a few seconds.")
-  engine.requestmaster(string.format("regserv %d\n", cs.serverport)) --this will fail in 15 seconds and causes our removal
-  engine.flushmasteroutput()
   engine.flushserver(true)
   engine.enet_host_flush(engine.serverhost)
   os.execute("sleep .1")
