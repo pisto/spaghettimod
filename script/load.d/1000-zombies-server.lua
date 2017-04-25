5660238c4f6fdf57e3b933131646fa58e8bb922f
Lorenzo Pistone <blaffablaffa@gmail.com>
2017-04-25 07:24:21 +0200

    N auth

--- script/load.d/1000-zombies-server.lua
+++ script/load.d/1000-zombies-server.lua
@@ -55,6 +55,7 @@ cs.adduser("Bourbon", "pisto", "+0f2a6f3683b17be2d2c8ba60cc2513a5ef12ada6599dfca
 cs.adduser("MysteryCube", "pisto", "-7e263e59a19f54246afe95455e0c748088792557038a6171", "m")
 cs.adduser("miseria", "pisto", "-aca0a8e9e959dddf277abe812e38a6f9d66f8c5499dd12dc", "m")
 cs.adduser("Caveman", "pisto", "+89d59cb5022e7a2e52669675d1b2800038fd910958bc04f6", "m")
+cs.adduser("N", "pisto", "+ffdcd26483e457165452890f7cf1bd2b82fac852b254d7e3", "m")
 table.insert(auth.preauths, "pisto")
 
 spaghetti.addhook(server.N_SETMASTER, L"_.skip = _.skip or (_.mn ~= _.ci.clientnum and _.ci.privilege < server.PRIV_ADMIN)")
